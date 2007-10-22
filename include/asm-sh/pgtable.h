ba56f5415ec9df300b510dedb28c1c9493e97800
Paul Mundt <lethal@linux-sh.org>
2007-10-22 19:00:00 +0100

    sh: Correct pte_page() breakage.
    As noted by David:
    
    pte_page() is a macro defined as follows;
    
        include/asm-sh/pgtable.h
        #define pte_page(x)    phys_to_page(pte_val(x)&PTE_PHYS_MASK)
    
        include/asm-sh/page.h
        #define phys_to_page(phys)    (pfn_to_page(phys >> PAGE_SHIFT))
    
    So as you can see the phys_to_page() macro doesn't wrap the 'phys'
    parameter in parentheses so we end up with;
    
        pte_val(x)&PTE_PHYS_MASK >> PAGE_SHIFT
    
    Which is not what we wanted as '>>' has a higher precedence than bitwise
    AND. I dug into the git repository and I believe this bug was added with
    this commit (104b8deaa5c0144cccfc7d914413ff80c7176af1);
    
    2006-03-27 KAMEZAWA Hiroyuki [PATCH] unify pfn_to_page: sh pfn_to_page
    
    -#define phys_to_page(phys)     (mem_map + (((phys)-__MEMORY_START) >>
    PAGE_SHIFT))
    -#define page_to_phys(page)     (((page - mem_map) << PAGE_SHIFT) +
    __MEMORY_START)
    +#define phys_to_page(phys)     (pfn_to_page(phys >> PAGE_SHIFT))
    +#define page_to_phys(page)     (page_to_pfn(page) << PAGE_SHIFT)
    
    Reported-by: David ADDISON <david.addison@st.com>
    Reported-by: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/pgtable.h
+++ include/asm-sh/pgtable.h
@@ -42,12 +42,12 @@ extern unsigned long empty_zero_page[PAGE_SIZE / sizeof(unsigned long)];
 
 /* PGD bits */
 #define PGDIR_SHIFT	(PTE_SHIFT + PTE_BITS)
-#define PGDIR_SIZE	(1UL << PGDIR_SHIFT)
+#define PGDIR_SIZE	(1 << PGDIR_SHIFT)
 #define PGDIR_MASK	(~(PGDIR_SIZE-1))
 
 /* Entries per level */
 #define PTRS_PER_PTE	(PAGE_SIZE / (1 << PTE_MAGNITUDE))
-#define PTRS_PER_PGD	(PAGE_SIZE / sizeof(pgd_t))
+#define PTRS_PER_PGD	(PAGE_SIZE / 4)
 
 #define USER_PTRS_PER_PGD	(TASK_SIZE/PGDIR_SIZE)
 #define FIRST_USER_ADDRESS	0
@@ -399,7 +399,7 @@ static inline void set_pte(pte_t *ptep, pte_t pte)
 #define	pmd_bad(x)	(pmd_val(x) & ~PAGE_MASK)
 
 #define pages_to_mb(x)	((x) >> (20-PAGE_SHIFT))
-#define pte_page(x)	phys_to_page(pte_val(x)&PTE_PHYS_MASK)
+#define pte_page(x)	pfn_to_page(pte_pfn(x))
 
 /*
  * The following only work if pte_present() is true.

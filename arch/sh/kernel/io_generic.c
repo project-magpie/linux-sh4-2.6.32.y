23ad354bb2ebc242ee5e3a2f5f0a75dd511e43fb
Stuart Menefy <stuart.menefy@st.com>
2007-10-22 19:00:00 +0100

    sh: Optimise generic_in/outsl for SH4.
    Optimise generic_in/outsl. After performing the port2addr conversion,
    and checking that the data is correctly aligned, simply call
    readsl/writesl. These have already been optimised.
    
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>

--- arch/sh/kernel/io_generic.c
+++ arch/sh/kernel/io_generic.c
@@ -104,8 +104,7 @@ void generic_insl(unsigned long port, void *dst, unsigned long count)
 	u32 *buf = dst;
 
 	port_addr = (volatile u32 *)ioport_map(port, 4);
-	while (count--)
-		*buf++ = *port_addr;
+	__raw_readsl((void __iomem*)port, buf, count);
 
 	dummy_read();
 }
@@ -178,8 +177,7 @@ void generic_outsl(unsigned long port, const void *src, unsigned long count)
 	const u32 *buf = src;
 
 	port_addr = (volatile u32 __force *)ioport_map(port, 4);
-	while (count--)
-		*port_addr = *buf++;
+	__raw_writesl((void __iomem*)port, buf, count);
 
 	dummy_read();
 }

7a8fb153385fab65f0533e014b6a026d436607c2
Francesco VIRLINZI <francesco.virlinzi@st.com>
2009-05-11 11:37:09 +0200

    stm: pm: Added SATA PM capability on 7200c3
    Added SATA PM specific ca[ability to the 7200 c3
    
    Signed-off-by: Francesco Virlinzi <francesco.virlinzi@st.com>

--- arch/sh/kernel/cpu/sh4/platform-pm-stx7200.c
+++ arch/sh/kernel/cpu/sh4/platform-pm-stx7200.c
@@ -74,10 +74,44 @@ emi_pwd_dwn_ack(struct platform_device *pdev, int host_phy, int ack)
 	return -EINVAL;
 }
 
+static int
+sata_pwd_dwn_req(struct platform_device *pdev, int host_phy, int pwd)
+{
+	static struct sysconf_field *sc[2];
+	if (cpu_data->cut_major < 3)
+		return 0;
+
+	if (!sc[pdev->id])
+		sc[pdev->id] = sysconf_claim(SYS_CFG, 22,
+			1 + pdev->id, 1 + pdev->id, "sata");
+	sysconf_write(sc[pdev->id], (pwd ? 1 : 0));
+	return 0;
+}
+
+static int
+sata_pwd_dwn_ack(struct platform_device *pdev, int host_phy, int ack)
+{
+	static struct sysconf_field *sc[2];
+	int i;
+	if (cpu_data->cut_major < 3)
+		return 0;
+	if (!sc[pdev->id])
+		sc[pdev->id] = sysconf_claim(SYS_STA, 13,
+			0 + pdev->id, 0 + pdev->id, "sata");
+	for (i = 5; i; --i) {
+		if (sysconf_read(sc[pdev->id]) == ack)
+			return 0;
+		mdelay(10);
+	}
+	return -EINVAL;
+}
+
 static struct platform_device_pm stx7200_pm_devices[] = {
 pm_plat_name("emi", NULL, emi_pwd_dwn_req, emi_pwd_dwn_ack, NULL),
 pm_plat_dev(&st_usb[0], NULL, usb_pwr_dwn, usb_pwr_ack, usb_sw_reset),
 pm_plat_dev(&st_usb[1], NULL, usb_pwr_dwn, usb_pwr_ack, usb_sw_reset),
 pm_plat_dev(&st_usb[2], NULL, usb_pwr_dwn, usb_pwr_ack, usb_sw_reset),
+pm_plat_dev(&sata_device[0], NULL, sata_pwd_dwn_req, sata_pwd_dwn_ack, NULL),
+pm_plat_dev(&sata_device[1], NULL, sata_pwd_dwn_req, sata_pwd_dwn_ack, NULL),
 };
 #endif

2a3719e2046c7a5f8a1158c403a53032ebc11d0a
Giuseppe CAVALLARO <peppe.cavallaro@st.com>
2009-06-15 09:06:24 +0200

    sh: fix sys_cacheflush error checking
    sys_cacheflush should return with EINVAL if the cache parameter is not
    one of ICACHE, DCACHE or BCACHE.
    So, we need to include 0 in the first check.
    
    It also adds the three definitions above as wrapper of the existent macros.
    
    PS: ltp cacheflush01 test now passes.
    
    Signed-off-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>

--- arch/sh/kernel/sys_sh.c
+++ arch/sh/kernel/sys_sh.c
@@ -189,7 +189,7 @@ sys_cacheflush (unsigned long addr, unsigned long len, int op)
 {
 	struct vm_area_struct *vma;
 
-	if ((op < 0) || (op > (CACHEFLUSH_D_PURGE|CACHEFLUSH_I)))
+	if ((op <= 0) || (op > (CACHEFLUSH_D_PURGE|CACHEFLUSH_I)))
 		return -EINVAL;
 
 	/*

64c7c4266eeedd5bf356b9981604c0d2a76abdc5
Stuart Menefy <stuart.menefy@st.com>
2009-10-27 15:14:18 +0000

    sh_stm: mb839 BSP update
    BSP for the mb839 board updated to the new style.
    
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>

--- arch/sh/boards/mach-mb839/setup.c
+++ arch/sh/boards/mach-mb839/setup.c
@@ -12,107 +12,61 @@
 
 #include <linux/init.h>
 #include <linux/platform_device.h>
-#include <linux/leds.h>
-#include <linux/stm/pio.h>
-#include <linux/stm/soc.h>
-#include <linux/stm/emi.h>
 #include <linux/delay.h>
+#include <linux/io.h>
+#include <linux/phy.h>
+#include <linux/leds.h>
+#include <linux/lirc.h>
+#include <linux/gpio.h>
+#include <linux/gpio_keys.h>
+#include <linux/input.h>
+#include <linux/stm/platform.h>
+#include <linux/stm/stx7105.h>
+#include <linux/stm/pci-synopsys.h>
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/physmap.h>
 #include <linux/mtd/partitions.h>
-#include <linux/phy.h>
-#include <linux/gpio_keys.h>
-#include <linux/input.h>
-#include <linux/irq.h>
 #include <asm/irq-ilc.h>
-#include <asm/irl.h>
-#include <asm/io.h>
-#include <mach/common.h>
 
-/* smart card on PIO0, modem on PIO4, uart on PIO5 */
-static int ascs[2] __initdata = { 2, 3 };
+#define MB839_PIO_PHY_NOTINTMODE	stm_gpio(7, 0)
+#define MB839_PIO_PHY_MODE1		stm_gpio(7, 1)
+#define MB839_PIO_PHY_MODE2		stm_gpio(7, 2)
+#define MB839_PIO_PHY_RESET		stm_gpio(7, 3)
 
 static void __init mb839_setup(char **cmdline_p)
 {
 	printk("STMicroelectronics MB839 initialisation\n");
 
 	stx7105_early_device_init();
-	stx7105_configure_asc(ascs, 2, 1);
+
+	/* smart card on PIO0, modem on PIO4, uart on PIO5 */
+	stx7105_configure_asc(2, &(struct stx7105_asc_config) {
+			.routing.asc2 = stx7105_asc2_pio4,
+			.hw_flow_control = 1,
+			.is_console = 0, });
+	stx7105_configure_asc(3, &(struct stx7105_asc_config) {
+			.hw_flow_control = 1,
+			.is_console = 1, });
 }
-
-static struct plat_stm_pwm_data pwm_private_info = {
-	.flags = PLAT_STM_PWM_OUT0,
-	.routing = PWM_OUT0_PIO13_0,
-};
-
-static struct plat_ssc_data ssc_private_info = {
-	.capability =
-	    ssc0_has(SSC_I2C_CAPABILITY) |
-	    ssc1_has(SSC_I2C_CAPABILITY) |
-	    ssc2_has(SSC_I2C_CAPABILITY) | ssc3_has(SSC_I2C_CAPABILITY),
-	.routing = SSC3_SCLK_PIO3_6 | SSC3_MTSR_PIO3_7 | SSC3_MRST_PIO3_7,
-};
-
-static struct usb_init_data usb_init[2] __initdata = {
-	{
-	 .oc_en = 1,
-	 .oc_actlow = 0,
-	 .oc_pinsel = USB0_OC_PIO4_4,
-	 .pwr_en = 1,
-	 .pwr_pinsel = USB0_PWR_PIO4_5,
-	 }, {
-	     .oc_en = 1,
-	     .oc_actlow = 0,
-	     .oc_pinsel = USB1_OC_PIO4_6,
-	     .pwr_en = 1,
-	     .pwr_pinsel = USB1_PWR_PIO4_7,
-	     }
-};
 
 static struct platform_device mb839_leds = {
 	.name = "leds-gpio",
 	.id = 0,
 	.dev.platform_data = &(struct gpio_led_platform_data){
-							      .num_leds = 2,
-							      .leds =
-							      (struct
-							       gpio_led[]){
-									   {
-									    .
-									    name
-									    =
-									    "LD1",
-									    .
-									    default_trigger
-									    =
-									    "heartbeat",
-									    .
-									    gpio
-									    =
-									    stpio_to_gpio
-									    (2,
-									     0),
-									    .
-									    active_low
-									    = 1,
-									    },
-									   {
-									    .
-									    name
-									    =
-									    "LD2",
-									    .
-									    gpio
-									    =
-									    stpio_to_gpio
-									    (2,
-									     1),
-									    .
-									    active_low
-									    = 1,
-									    },
-									   },
-							      },
+		.num_leds = 2,
+		.leds = (struct gpio_led[]){
+			{
+				.name 		= "LD1",
+				.default_trigger= "heartbeat",
+				.gpio		= stm_gpio(2, 0),
+				.active_low	= 1,
+			}, {
+				.name		= "LD2",
+				.gpio		= stm_gpio(2, 1),
+				.active_low	= 1,
+			},
+		},
+	},
 };
 
 /*
@@ -120,25 +74,24 @@ static struct platform_device mb839_leds = {
  * pins.  Some e/net lines are also used as SoC mode pins and we need to
  * drive these high in order to get e/net to work.
  */
-static struct stpio_pin *phy_reset, *phy_mode1, *phy_mode2, *phy_notintmode;
 
 static int mb839_phy_reset(void *bus)
 {
-	stpio_set_pin(phy_notintmode, 1);
-	stpio_set_pin(phy_mode1, 1);
-	stpio_set_pin(phy_mode2, 1);
+	gpio_set_value(MB839_PIO_PHY_NOTINTMODE, 1);
+	gpio_set_value(MB839_PIO_PHY_MODE1, 1);
+	gpio_set_value(MB839_PIO_PHY_MODE2, 1);
 
-	stpio_set_pin(phy_reset, 1);
+	gpio_set_value(MB839_PIO_PHY_RESET, 1);
 	udelay(1);
-	stpio_set_pin(phy_reset, 0);
+	gpio_set_value(MB839_PIO_PHY_RESET, 0);
 	udelay(100);
-	stpio_set_pin(phy_reset, 1);
+	gpio_set_value(MB839_PIO_PHY_RESET, 1);
 	udelay(1);
 
 	return 0;
 }
 
-static struct plat_stmmacphy_data phy_private_data = {
+static struct stm_plat_stmmacphy_data mb839_phy_private_data = {
 	/* SMSC 8700 */
 	.bus_id = 0,
 	.phy_addr = -1,
@@ -152,88 +105,114 @@ static struct platform_device mb839_phy_device = {
 	.id = 0,
 	.num_resources = 1,
 	.resource = (struct resource[]){
-					{
-					 .name = "phyirq",
-					 .start = -1,	/*FIXME, should be ILC_EXT_IRQ(6), */
-					 .end = -1,
-					 .flags = IORESOURCE_IRQ,
-					 },
-					},
+		{
+			.name = "phyirq",
+			.start = -1,	/*FIXME, should be ILC_EXT_IRQ(6), */
+			.end = -1,
+			.flags = IORESOURCE_IRQ,
+		},
+	},
 	.dev = {
-		.platform_data = &phy_private_data,
-		}
+		.platform_data = &mb839_phy_private_data,
+	}
 };
 
-static struct mtd_partition mtd_parts_table[] = {
+static struct mtd_partition mb839_mtd_parts_table[] = {
 	{
-	 .name = "NOR FLASH",
-	 .size = MTDPART_SIZ_FULL,
-	 .offset = 0x00000000,
+		.name = "NOR FLASH",
+		.size = MTDPART_SIZ_FULL,
+		.offset = 0x00000000,
 	 }
 };
 
-static struct physmap_flash_data physmap_flash_data = {
+static struct physmap_flash_data mb839_physmap_flash_data = {
 	.width = 2,
-	.nr_parts = ARRAY_SIZE(mtd_parts_table),
-	.parts = mtd_parts_table
+	.nr_parts = ARRAY_SIZE(mb839_mtd_parts_table),
+	.parts = mb839_mtd_parts_table
 };
 
-static struct platform_device physmap_flash = {
+static struct platform_device mb839_physmap_flash = {
 	.name = "physmap-flash",
 	.id = -1,
 	.num_resources = 1,
 	.resource = (struct resource[]){
-					{
-					 .start = 0x00000000,	/* Can be overridden */
-					 .end = 128 * 1024 * 1024 - 1,
-					 .flags = IORESOURCE_MEM,
-					 }
-					},
+		{
+			.start = 0x00000000,	/* Can be overridden */
+			.end = 128 * 1024 * 1024 - 1,
+			.flags = IORESOURCE_MEM,
+		}
+	},
 	.dev = {
-		.platform_data = &physmap_flash_data,
-		},
+		.platform_data = &mb839_physmap_flash_data,
+	},
 };
 
 static struct platform_device *mb839_devices[] __initdata = {
 	&mb839_leds,
 	&mb839_phy_device,
-	&physmap_flash,
+	&mb839_physmap_flash,
 };
 
 static int __init device_init(void)
 {
 	stx7105_configure_sata();
-	stx7105_configure_pwm(&pwm_private_info);
-	stx7105_configure_ssc(&ssc_private_info);
 
-	stx7105_configure_usb(0, &usb_init[0]);
-	stx7105_configure_usb(1, &usb_init[1]);
+	stx7105_configure_pwm(&(struct stx7105_pwm_config) {
+			.out0 = stx7105_pwm_out0_pio13_0,
+			.out1 = stx7105_pwm_out1_disabled, });
 
-	phy_notintmode = stpio_request_pin(7, 0, "PhyNotIntMode", STPIO_OUT);
-	phy_mode1 = stpio_request_pin(7, 1, "PhyMode1Sel", STPIO_OUT);
-	phy_mode2 = stpio_request_pin(7, 2, "PhyMode2Sel", STPIO_OUT);
-	phy_reset = stpio_request_pin(7, 3, "ResetMII", STPIO_OUT);
+	stx7105_configure_ssc_i2c(1, &(struct stx7105_ssc_config) {
+			.routing.ssc1.sclk = stx7105_ssc1_sclk_pio2_5,
+			.routing.ssc1.mtsr = stx7105_ssc1_mtsr_pio2_6, });
+	stx7105_configure_ssc_i2c(2, &(struct stx7105_ssc_config) {
+			.routing.ssc2.sclk = stx7105_ssc2_sclk_pio3_4,
+			.routing.ssc2.mtsr = stx7105_ssc2_mtsr_pio3_5, });
+	stx7105_configure_ssc_i2c(3, &(struct stx7105_ssc_config) {
+			.routing.ssc3.sclk = stx7105_ssc3_sclk_pio3_6,
+			.routing.ssc3.mtsr = stx7105_ssc3_mtsr_pio3_7, });
 
-	stx7105_configure_ethernet(0, 0, 0, 0, 1, 0);
+	stx7105_configure_usb(0, &(struct stx7105_usb_config) {
+			.ovrcur_mode = stx7105_usb_ovrcur_active_high,
+			.pwr_enabled = 1,
+			.routing.usb0.ovrcur = stx7105_usb0_ovrcur_pio4_4,
+			.routing.usb0.pwr = stx7105_usb0_pwr_pio4_5, });
+	stx7105_configure_usb(1, &(struct stx7105_usb_config) {
+			.ovrcur_mode = stx7105_usb_ovrcur_active_high,
+			.pwr_enabled = 1,
+			.routing.usb1.ovrcur = stx7105_usb1_ovrcur_pio4_6,
+			.routing.usb1.pwr = stx7105_usb1_pwr_pio4_7, });
 
-	stx7105_configure_lirc();
+	gpio_request(MB839_PIO_PHY_NOTINTMODE, "PhyNotIntMode");
+	gpio_request(MB839_PIO_PHY_MODE1, "PhyMode1Sel");
+	gpio_request(MB839_PIO_PHY_MODE2, "PhyMode2Sel");
+	gpio_request(MB839_PIO_PHY_RESET, "ResetMII");
+
+	stx7105_configure_ethernet(&(struct stx7105_ethernet_config) {
+			.mode = stx7105_ethernet_mode_mii,
+			.ext_clk = 1,
+			.phy_bus = 0, });
+
+	stx7105_configure_lirc(&(struct stx7105_lirc_config) {
+			.rx_mode = stx7105_lirc_rx_mode_ir,
+			.tx_enabled = 1,
+			.tx_od_enabled = 1, });
 
 	return platform_add_devices(mb839_devices, ARRAY_SIZE(mb839_devices));
 }
 
 arch_initcall(device_init);
 
-#ifndef CONFIG_GENERIC_IOMAP
 static void __iomem *mb839_ioport_map(unsigned long port, unsigned int size)
 {
-	/* However picking somewhere safe isn't as easy as you might think.
-	 * I used to use external ROM, but that can cause problems if you are
-	 * in the middle of updating Flash. So I'm now using the processor core
-	 * version register, which is guaranted to be available, and non-writable.
+	/*
+	 * If we have PCI then this should never be called because we
+	 * are using the generic iomap implementation. If we don't
+	 * have PCI then there are no IO mapped devices, so it still
+	 * shouldn't be called.
 	 */
+	BUG();
 	return (void __iomem *)CCN_PVR;
 }
-#endif
 
 static void __init mb839_init_irq(void)
 {
@@ -243,5 +222,12 @@ static void __init mb839_init_irq(void)
 }
 
 struct sh_machine_vector mv_mb839 __initmv = {
-	STM_MACHINE_VEC(mb839)
+	.mv_name		= "MB839 Reference Board",
+	.mv_setup		= mb839_setup,
+	.mv_nr_irqs		= NR_IRQS,
+	.mv_init_irq		= mb839_init_irq,
+	.mv_ioport_map		= mb839_ioport_map,
+#ifdef CONFIG_SH_ST_SYNOPSYS_PCI
+	STM_PCI_IO_MACHINE_VEC
+#endif
 };

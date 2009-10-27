ab3d2c1965ee5007f82e3e0e46aac5a65f6dfe75
Pawel Moll <pawel.moll@st.com>
2009-10-27 15:14:14 +0000

    sh_stm: hms1 BSP update.
    BSP for the hms1 board updated to the new style.
    
    Signed-off-by: Pawel Moll <pawel.moll@st.com>
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>

--- arch/sh/boards/mach-hms1/setup.c
+++ arch/sh/boards/mach-hms1/setup.c
@@ -12,42 +12,43 @@
 
 #include <linux/init.h>
 #include <linux/platform_device.h>
-#include <linux/stm/pio.h>
-#include <linux/stm/soc.h>
+#include <linux/gpio.h>
+#include <linux/lirc.h>
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/physmap.h>
 #include <linux/mtd/partitions.h>
+#include <linux/stm/platform.h>
+#include <linux/stm/stx7100.h>
 #include <asm/irl.h>
 
-static struct stpio_pin *vpp_pio;
 
-static int ascs[2] __initdata = { 2, 3 };
+
+#define HMS1_PIO_FLASH_VPP stm_gpio(2, 5)
+
+
 
 void __init hms1_setup(char** cmdline_p)
 {
 	printk("HMS1 board initialisation\n");
 
 	stx7100_early_device_init();
-	stb7100_configure_asc(ascs, 2, 0);
+
+	stx7100_configure_asc(2, &(struct stx7100_asc_config) {
+			.hw_flow_control = 0,
+			.is_console = 1, });
+	stx7100_configure_asc(3, &(struct stx7100_asc_config) {
+			.hw_flow_control = 0,
+			.is_console = 0, });
 }
 
-static struct plat_stm_pwm_data pwm_private_info = {
-	.flags		= PLAT_STM_PWM_OUT1,
-};
 
-static struct plat_ssc_data ssc_private_info = {
-	.capability  =
-		ssc0_has(SSC_I2C_CAPABILITY) |
-		ssc1_has(SSC_I2C_CAPABILITY) |
-		ssc2_has(SSC_I2C_CAPABILITY),
-};
 
-static void set_vpp(struct map_info * info, int enable)
+static void hms1_set_vpp(struct map_info *info, int enable)
 {
-	stpio_set_pin(vpp_pio, enable);
+	gpio_set_value(HMS1_PIO_FLASH_VPP, enable);
 }
 
-static struct mtd_partition mtd_parts_table[3] = {
+static struct mtd_partition hms1_mtd_parts_table[3] = {
 	{
 		.name = "Boot firmware",
 		.size = 0x00040000,
@@ -64,30 +65,30 @@ static struct mtd_partition mtd_parts_table[3] = {
 	}
 };
 
-static struct physmap_flash_data physmap_flash_data = {
+static struct physmap_flash_data hms1_physmap_flash_data = {
 	.width		= 2,
-	.set_vpp	= set_vpp,
-	.nr_parts	= ARRAY_SIZE(mtd_parts_table),
-	.parts		= mtd_parts_table
+	.set_vpp	= hms1_set_vpp,
+	.nr_parts	= ARRAY_SIZE(hms1_mtd_parts_table),
+	.parts		= hms1_mtd_parts_table
 };
 
-static struct resource physmap_flash_resource = {
+static struct resource hms1_physmap_flash_resource = {
 	.start		= 0x00000000,
 	.end		= 0x00800000 - 1,
 	.flags		= IORESOURCE_MEM,
 };
 
-static struct platform_device physmap_flash = {
+static struct platform_device hms1_physmap_flash = {
 	.name		= "physmap-flash",
 	.id		= -1,
 	.dev		= {
-		.platform_data	= &physmap_flash_data,
+		.platform_data	= &hms1_physmap_flash_data,
 	},
 	.num_resources	= 1,
-	.resource	= &physmap_flash_resource,
+	.resource	= &hms1_physmap_flash_resource,
 };
 
-static struct platform_device smsc_lan9117 = {
+static struct platform_device hms1_smsc_lan9117 = {
 	.name		= "smc911x",
 	.id		= -1,
 	.num_resources	= 4,
@@ -120,22 +121,40 @@ static struct platform_device smsc_lan9117 = {
 };
 
 static struct platform_device *hms1_devices[] __initdata = {
-	&physmap_flash,
-	&smsc_lan9117,
+	&hms1_physmap_flash,
+	&hms1_smsc_lan9117,
 };
 
 static int __init hms1_device_init(void)
 {
 	stx7100_configure_sata();
-	stx7100_configure_pwm(&pwm_private_info);
-	stx7100_configure_ssc(&ssc_private_info);
-	stx7100_configure_usb();
-	stx7100_configure_pata(3, 1, IRL1_IRQ);
-	stx7100_configure_lirc();
 
-	vpp_pio = stpio_request_pin(2,5, "VPP", STPIO_OUT);
+	stx7100_configure_pwm(&(struct stx7100_pwm_config) {
+			.out0_enabled = 0,
+			.out1_enabled = 1, });
+
+	stx7100_configure_ssc_i2c(0);
+	stx7100_configure_ssc_i2c(1);
+	stx7100_configure_ssc_i2c(2);
+
+	stx7100_configure_usb();
+
+	stx7100_configure_pata(&(struct stx7100_pata_config) {
+			.emi_bank = 3,
+			.pc_mode = 1,
+			.irq = IRL1_IRQ, });
+
+	/* HMS1 has no on-board IR receivers/transmitters. Anyway we still
+	 * configure them because receiver can be present on a front-panel
+	 * add-on and transmitter can be plugged to J9 mini-jack connector */
+	stx7100_configure_lirc(&(struct stx7100_lirc_config) {
+			.rx_mode = stx7100_lirc_rx_mode_ir,
+			.tx_enabled = 0,
+			.tx_od_enabled = 1, });
+
+	gpio_request(HMS1_PIO_FLASH_VPP, "Flash VPP");
+	gpio_direction_output(HMS1_PIO_FLASH_VPP, 0);
 
 	return platform_add_devices(hms1_devices, ARRAY_SIZE(hms1_devices));
 }
-
 arch_initcall(hms1_device_init);

a1efe361068a8fe66de9e04d7394fc4cc3ecd815
Pawel Moll <pawel.moll@st.com>
2009-10-27 15:14:15 +0000

    sh_stm: mb562 BSP updated.
    BSP for mb562 peripherals board updated to the new style.
    
    Signed-off-by: Pawel Moll <pawel.moll@st.com>
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>

--- arch/sh/boards/mach-st/mb562.c
+++ arch/sh/boards/mach-st/mb562.c
@@ -11,34 +11,21 @@
  */
 
 #include <linux/init.h>
-#include <linux/stm/sysconf.h>
+#include <linux/stm/stx7200.h>
 #include <linux/bug.h>
 #include <asm/processor.h>
 
 static int __init device_init(void)
 {
-	struct sysconf_field *sc;
-
 	/* So far valid only for 7200 processor board! */
 	BUG_ON(cpu_data->type != CPU_STX7200);
 
 	/* Set up "scenario 1" of audio outputs */
-
-	/* CONF_PAD_AUD[0] = 1
-	 * AUDDIG* are connected PCMOUT3_* - 10-channels PCM player #3 */
-	sc = sysconf_claim(SYS_CFG, 20, 0, 0, "pcm_player.3");
-	sysconf_write(sc, 1);
-
-	/* CONF_PAD_ETH[4] = 1
-	 * MII1RXD[3], MII1TXCLK, MII1COL, MII1CRS, MII1DINT & MII1PHYCL
-	 * connected to 6-channels PCM player #1 */
-	sc = sysconf_claim(SYS_CFG, 41, 20, 20, "pcm_player.1");
-	sysconf_write(sc, 1);
-
-	/* CONF_PAD_ETH[5] = 0
-	 * MII1CRS is output */
-	sc = sysconf_claim(SYS_CFG, 41, 21, 21, "pcm_player.1");
-	sysconf_write(sc, 0);
+	stx7200_configure_audio(&(struct stx7200_audio_config) {
+			.pcm_player_1_routing = stx7200_pcm_player_1_mii1,
+			.pcm_player_3_routing =
+					stx7200_pcm_player_3_aiddig0_auddig1,
+			});
 
 	return 0;
 }

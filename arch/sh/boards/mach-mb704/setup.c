c9c47c41e9331be767ea7c4917d230bb16ac5a61
Pawel Moll <pawel.moll@st.com>
2009-10-27 15:14:16 +0000

    sh_stm: mb704 BSP update.
    BSP for the mb704 board updated to the new style.
    
    Signed-off-by: Pawel Moll <pawel.moll@st.com>
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>

--- arch/sh/boards/mach-mb704/setup.c
+++ arch/sh/boards/mach-mb704/setup.c
@@ -12,56 +12,35 @@
 
 #include <linux/init.h>
 #include <linux/platform_device.h>
-#include <linux/leds.h>
-#include <linux/stm/pio.h>
-#include <linux/stm/soc.h>
-#include <linux/stm/emi.h>
-#include <linux/mtd/mtd.h>
-#include <linux/mtd/physmap.h>
-#include <linux/mtd/partitions.h>
-#include <linux/phy.h>
-#include <linux/spi/spi.h>
-#include <linux/spi/eeprom.h>
 #include <linux/io.h>
+#include <linux/leds.h>
+#include <linux/phy.h>
+#include <linux/gpio.h>
+#include <linux/stm/platform.h>
+#include <linux/stm/stx5197.h>
+#include <linux/mtd/mtd.h>
+#include <linux/mtd/partitions.h>
+#include <linux/spi/spi.h>
 #include <linux/irq.h>
+#include <linux/spi/flash.h>
 #include <asm/irq-ilc.h>
 #include <asm/irl.h>
-
-static int ascs[2] __initdata = { 2, 3 };
 
 static void __init mb704_setup(char **cmdline_p)
 {
 	printk(KERN_INFO "STMicroelectronics STx5197 Mboard initialisation\n");
 
 	stx5197_early_device_init();
-	stx5197_configure_asc(ascs, 2, 0);
+
+	stx5197_configure_asc(2, &(struct stx5197_asc_config) {
+			.hw_flow_control = 0,
+			.is_console = 1, });
+	stx5197_configure_asc(3, &(struct stx5197_asc_config) {
+			.hw_flow_control = 0,
+			.is_console = 0, });
 }
 
-static struct plat_stm_pwm_data pwm_private_info = {
-	.flags		= PLAT_STM_PWM_OUT0,
-};
 
-/*
- * Configure SSC0 as SPI to drive serial Flash attached to SPI (requires
- * SSC driver SPI) or as I2C to drive I2C devices such as HDMI.
- *
- * To use I2C bus 0 (PIO1:1:0]) it is necessary to fit jumpers
- * J2-E, J2-G, J2-F and J2-H.
- *
- * I2C bus 1 can be routed to on chip QPSK block (setting routing to
- * SSC1_QPSK) or the pins QAM_SCLT/SDAT (setting SSC1_QAM_SCLT_SDAT).
- * Both require SSC driver.
- *
- * To use I2C bus 2 (PIO3[3:2]) it is necessary to remove jumpers
- * J7-A, J7-C, J6-H and J6-F and fit J7-B and J6-G.
- */
-static struct plat_ssc_data ssc_private_info = {
-	.capability =
-		ssc0_has(SSC_SPI_CAPABILITY) |
-		ssc1_has(SSC_I2C_CAPABILITY) |
-		ssc2_has(SSC_I2C_CAPABILITY),
-	.routing = SSC1_QPSK,
-};
 
 static struct platform_device mb704_leds = {
 	.name = "leds-gpio",
@@ -72,14 +51,45 @@ static struct platform_device mb704_leds = {
 			{
 				.name = "HB",
 				.default_trigger = "heartbeat",
-				.gpio = stpio_to_gpio(3, 6),
+				.gpio = stm_gpio(3, 6),
 				.active_low = 1,
 			},
 		},
 	},
 };
 
-static struct plat_stmmacphy_data phy_private_data = {
+
+
+static struct mtd_partition mb704_serial_flash_partitions[] = {
+	{
+		.name = "sflash_1",
+		.size = 0x00080000,
+		.offset = 0,
+	}, {
+		.name = "sflash_2",
+		.size = MTDPART_SIZ_FULL,
+		.offset = 0x20000
+	},
+};
+
+/* Serial FLASH is AT45DB321x, handled by 'mtd_dataflash' SPI driver */
+static struct flash_platform_data mb704_serial_flash_data = {
+	.parts = mb704_serial_flash_partitions,
+	.nr_parts = ARRAY_SIZE(mb704_serial_flash_partitions),
+};
+
+/* Note to use the mb704's SPI Flash device J10 needs to be in position 1-2. */
+static struct spi_board_info mb704_serial_flash = {
+	.modalias       = "mtd_dataflash",
+	.chip_select    = 0,
+	.max_speed_hz   = 120000,
+	.bus_num        = 0,
+	.platform_data  = &mb704_serial_flash_data,
+};
+
+
+
+static struct stm_plat_stmmacphy_data mb704_phy_private_data = {
 	/* SMSC LAN 8700 on the mb762 */
 	.bus_id = 0,
 	.phy_addr = -1,
@@ -94,23 +104,12 @@ static struct platform_device mb704_phy_device = {
 	.resource	= (struct resource[]) {
 		{
 			.name	= "phyirq",
-			.start	= -1,/* FIXME should be ILC_IRQ(25) */
+			.start	= -1, /* FIXME should be ILC_IRQ(25) */
 			.end	= -1,
 			.flags	= IORESOURCE_IRQ,
 		},
 	},
-	.dev = {
-		.platform_data = &phy_private_data,
-	}
-};
-
-/* Note to use the mb704's SPI Flash device J10 needs to be in position 1-2. */
-
-static struct spi_board_info mb704_spi_device = {
-	.modalias       = "mtd_dataflash",
-	.chip_select    = 0,
-	.max_speed_hz   = 120000,
-	.bus_num        = 0,
+	.dev.platform_data = &mb704_phy_private_data,
 };
 
 static struct platform_device *mb704_devices[] __initdata = {
@@ -118,23 +117,47 @@ static struct platform_device *mb704_devices[] __initdata = {
 	&mb704_phy_device,
 };
 
-static int __init device_init(void)
+static int __init mb704_devices_init(void)
 {
-	/*
-	 * By default we don't configure PWM as the mb704 has J2C fitted
+	/* By default we don't configure PWM as the mb704 has J2C fitted
 	 * which resulsts in contention.
-	 * stx5197_configure_pwm(&pwm_private_info);
+	 * stx5197_configure_pwm();
 	 */
-	stx5197_configure_ssc(&ssc_private_info);
-	stx5197_configure_usb();
-	stx5197_configure_ethernet(0, 1, 0);
-	stx5197_configure_lirc();
 
-	spi_register_board_info(&mb704_spi_device, 1);
+	/* Configure SSC0 as SPI to drive serial Flash attached to SPI
+	 * (requires SSC driver SPI) or as I2C to drive I2C devices such
+	 * as HDMI.
+	 * To use SSC0 as I2C bus 0 (PIO1:1:0]) it is necessary to fit
+	 * jumpers J2-E, J2-G, J2-F and J2-H. */
+	stx5197_configure_ssc_spi(0);
+	/* I2C bus 1 can be routed to on chip QPSK block (setting routing to
+	 * STX7141_ROUTING_SSC1_I2C_QPSK) or the pins QAM_SCLT/SDAT
+	 * (setting STX7141_ROUTING_SSC1_I2C_QAM). Both require SSC driver. */
+	stx5197_configure_ssc_i2c(1, &(struct stx5197_ssc_i2c_config) {
+			.routing.ssc1 = stx5197_ssc1_i2c_qpsk, });
+	/* To use I2C bus 2 (PIO3[3:2]) it is necessary to remove jumpers
+	 * J7-A, J7-C, J6-H and J6-F and fit J7-B and J6-G. */
+	stx5197_configure_ssc_i2c(2, &(struct stx5197_ssc_i2c_config) {
+			.routing.ssc2 = stx5197_ssc2_i2c_pio3, });
+
+	stx5197_configure_usb();
+
+	stx5197_configure_ethernet(&(struct stx5197_ethernet_config) {
+			.mode = stx5197_ethernet_mode_mii,
+			.ext_clk = 1,
+			.phy_bus = 0, });
+
+	/* To have UHF available on MB704:
+	 * need one wire from J14-C to J14-E */
+	stx5197_configure_lirc(&(struct stx5197_lirc_config) {
+			.rx_mode = stx5197_lirc_rx_mode_ir,
+			.tx_enabled = 1, });
+
+	spi_register_board_info(&mb704_serial_flash, 1);
 
 	return platform_add_devices(mb704_devices, ARRAY_SIZE(mb704_devices));
 }
-arch_initcall(device_init);
+arch_initcall(mb704_devices_init);
 
 static void __iomem *mb704_ioport_map(unsigned long port, unsigned int size)
 {

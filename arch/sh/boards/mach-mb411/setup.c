9593b3fa83a8245bb2b372f1f33dff5e8ce7aa37
Pawel Moll <pawel.moll@st.com>
2009-10-27 15:14:14 +0000

    sh_stm: mb411 BSP update.
    BSP for the mb411 board updated to the new style.
    
    Signed-off-by: Pawel Moll <pawel.moll@st.com>
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>

--- arch/sh/boards/mach-mb411/setup.c
+++ arch/sh/boards/mach-mb411/setup.c
@@ -12,42 +12,40 @@
 
 #include <linux/init.h>
 #include <linux/platform_device.h>
-#include <linux/stm/pio.h>
-#include <linux/stm/soc.h>
-#include <linux/delay.h>
+#include <linux/gpio.h>
+#include <linux/phy.h>
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/physmap.h>
 #include <linux/mtd/partitions.h>
-#include <linux/phy.h>
+#include <linux/stm/platform.h>
+#include <linux/stm/stx7100.h>
 #include <sound/stm.h>
 #include <asm/io.h>
 #include <mach/epld.h>
-#include <asm/irq-stb7100.h>
 #include <asm/irq.h>
+#include <asm/irq-stx7100.h>
 #include <mach/common.h>
 
-static int ascs[2] __initdata = { 2, 3 };
+
+
 
 static void __init mb411_setup(char** cmdline_p)
 {
 	printk("STMicroelectronics STb7100 MBoard board initialisation\n");
 
-        stx7100_early_device_init();
-        stb7100_configure_asc(ascs, 2, 0);
+	stx7100_early_device_init();
+
+	stx7100_configure_asc(2, &(struct stx7100_asc_config) {
+			.hw_flow_control = 1,
+			.is_console = 1, });
+	stx7100_configure_asc(3, &(struct stx7100_asc_config) {
+			.hw_flow_control = 1,
+			.is_console = 0, });
 }
 
-static struct plat_stm_pwm_data pwm_private_info = {
-	.flags		= PLAT_STM_PWM_OUT0 | PLAT_STM_PWM_OUT1,
-};
 
-static struct plat_ssc_data ssc_private_info = {
-	.capability  =
-		ssc0_has(SSC_I2C_CAPABILITY) |
-		ssc1_has(SSC_SPI_CAPABILITY) |
-		ssc2_has(SSC_I2C_CAPABILITY),
-};
 
-static struct resource smc91x_resources[] = {
+static struct resource mb411_smc91x_resources[] = {
 	[0] = {
 		.start	= 0x03e00300,
 		.end	= 0x03e00300 + 0xff,
@@ -60,14 +58,16 @@ static struct resource smc91x_resources[] = {
 	},
 };
 
-static struct platform_device smc91x_device = {
+static struct platform_device mb411_smc91x_device = {
 	.name		= "smc91x",
 	.id		= 0,
-	.num_resources	= ARRAY_SIZE(smc91x_resources),
-	.resource	= smc91x_resources,
+	.num_resources	= ARRAY_SIZE(mb411_smc91x_resources),
+	.resource	= mb411_smc91x_resources,
 };
 
-static struct mtd_partition mtd_parts_table[3] = {
+
+
+static struct mtd_partition mb411_mtd_parts_table[3] = {
 	{
 		.name = "Boot firmware",
 		.size = 0x00040000,
@@ -83,7 +83,7 @@ static struct mtd_partition mtd_parts_table[3] = {
 	}
 };
 
-static void mtd_set_vpp(struct map_info *map, int vpp)
+static void mb411_mtd_set_vpp(struct map_info *map, int vpp)
 {
 	/* Bit 0: VPP enable
 	 * Bit 1: Reset (not used in later EPLD versions)
@@ -96,54 +96,58 @@ static void mtd_set_vpp(struct map_info *map, int vpp)
 	}
 }
 
-static struct physmap_flash_data physmap_flash_data = {
+static struct physmap_flash_data mb411_physmap_flash_data = {
 	.width		= 2,
-	.set_vpp	= mtd_set_vpp,
-	.nr_parts	= ARRAY_SIZE(mtd_parts_table),
-	.parts		= mtd_parts_table
+	.set_vpp	= mb411_mtd_set_vpp,
+	.nr_parts	= ARRAY_SIZE(mb411_mtd_parts_table),
+	.parts		= mb411_mtd_parts_table
 };
 
-static struct resource physmap_flash_resource = {
+static struct resource mb411_physmap_flash_resource = {
 	.start		= 0x00000000,
 	.end		= 0x00800000 - 1,
 	.flags		= IORESOURCE_MEM,
 };
 
-static struct platform_device physmap_flash = {
+static struct platform_device mb411_physmap_flash = {
 	.name		= "physmap-flash",
 	.id		= -1,
 	.dev		= {
-		.platform_data	= &physmap_flash_data,
+		.platform_data	= &mb411_physmap_flash_data,
 	},
 	.num_resources	= 1,
-	.resource	= &physmap_flash_resource,
+	.resource	= &mb411_physmap_flash_resource,
 };
 
-static struct plat_stmmacphy_data phy_private_data = {
-        .bus_id = 0,
-        .phy_addr = 0,
-        .phy_mask = 0,
-        .interface = PHY_INTERFACE_MODE_MII,
+
+
+static struct stm_plat_stmmacphy_data mb411_phy_private_data = {
+	.bus_id = 0,
+	.phy_addr = -1,
+	.phy_mask = 0,
+	.interface = PHY_INTERFACE_MODE_MII,
 };
 
 static struct platform_device mb411_phy_device = {
-        .name           = "stmmacphy",
-        .id             = 0,
-        .num_resources  = 1,
-        .resource       = (struct resource[]) {
-                {
-                        .name   = "phyirq",
-                        .start  = 0,
-                        .end    = 0,
-                        .flags  = IORESOURCE_IRQ,
-                },
-        },
-        .dev = {
-                .platform_data = &phy_private_data,
-         }
+	.name           = "stmmacphy",
+	.id             = 0,
+	.num_resources  = 1,
+	.resource       = (struct resource[]) {
+		{
+			.name   = "phyirq",
+			.start  = 0,
+			.end    = 0,
+			.flags  = IORESOURCE_IRQ,
+		},
+	},
+	.dev = {
+		.platform_data = &mb411_phy_private_data,
+	}
 };
 
-static struct platform_device epld_device = {
+
+
+static struct platform_device mb411_epld_device = {
 	.name		= "epld",
 	.id		= -1,
 	.num_resources	= 1,
@@ -159,7 +163,8 @@ static struct platform_device epld_device = {
 	},
 };
 
-#ifdef CONFIG_SND
+
+
 /* Unfortunately PDN and SMUTE can't be controlled using
  * default audio EPLD "firmware"...
  * That is why dummy converter is enough. */
@@ -176,43 +181,56 @@ static struct platform_device mb411_snd_ext_dacs = {
 		.oversampling = 256,
 	},
 };
-#endif
+
+
 
 static struct platform_device *mb411_devices[] __initdata = {
-	&epld_device,
-	&smc91x_device,
-	&physmap_flash,
+	&mb411_epld_device,
+	&mb411_smc91x_device,
+	&mb411_physmap_flash,
 	&mb411_phy_device,
-#ifdef CONFIG_SND
 	&mb411_snd_ext_dacs,
-#endif
 };
 
-static int __init device_init(void)
+static int __init mb411_device_init(void)
 {
 	stx7100_configure_sata();
-	stx7100_configure_pwm(&pwm_private_info);
-	stx7100_configure_ssc(&ssc_private_info);
-	stx7100_configure_usb();
-	stx7100_configure_lirc();
-	stx7100_configure_ethernet(0, 0, 0);
 
-#ifdef CONFIG_PATA_PLATFORM
+	stx7100_configure_pwm(&(struct stx7100_pwm_config) {
+			.out0_enabled = 1,
+			.out1_enabled = 1, });
+
+	stx7100_configure_ssc_i2c(0);
+	stx7100_configure_ssc_spi(1, NULL);
+	stx7100_configure_ssc_i2c(2);
+
+	stx7100_configure_usb();
+
+	stx7100_configure_lirc(&(struct stx7100_lirc_config) {
+			.rx_mode = stx7100_lirc_rx_mode_ir,
+			.tx_enabled = 1,
+			.tx_od_enabled = 1, });
+
+	stx7100_configure_ethernet(&(struct stx7100_ethernet_config) {
+			.mode = stx7100_ethernet_mode_mii,
+			.ext_clk = 0,
+			.phy_bus = 0, });
+
 	/* Set the EPLD ATAPI register to 1, enabling the IDE interface.*/
 	epld_write(1, EPLD_ATAPI);
-	stx7100_configure_pata(3, 1, 8);
-#endif
+	stx7100_configure_pata(&(struct stx7100_pata_config) {
+			.emi_bank = 3,
+			.pc_mode = 1,
+			.irq = 8, });
 
-#ifdef CONFIG_SND
 	/* Initialize audio EPLD */
 	epld_write(0x1, EPLD_DAC_PNOTS);
 	epld_write(0x7, EPLD_DAC_SPMUX);
-#endif
 
 	return platform_add_devices(mb411_devices,
-                                    ARRAY_SIZE(mb411_devices));
+			ARRAY_SIZE(mb411_devices));
 }
-device_initcall(device_init);
+device_initcall(mb411_device_init);
 
 static void __iomem *mb411_ioport_map(unsigned long port, unsigned int size)
 {
@@ -229,7 +247,7 @@ static void __init mb411_init_irq(void)
 	unsigned long epldver;
 	unsigned long pcbver;
 
-	epld_early_init(&epld_device);
+	epld_early_init(&mb411_epld_device);
 
 	epldver = epld_read(EPLD_EPLDVER);
 	pcbver = epld_read(EPLD_PCBVER);
@@ -242,7 +260,7 @@ static void __init mb411_init_irq(void)
 	ilc_route_external(ILC_EXT_IRQ1, 5, 0);
 	ilc_route_external(ILC_EXT_IRQ2, 6, 0);
 
-        /* Route e/net PHY interrupt to SH4 - only for STb7109 */
+	/* Route e/net PHY interrupt to SH4 - only for STb7109 */
 #ifdef CONFIG_STMMAC_ETH
 	/* Note that we invert the signal - the ste101p is connected
 	   to the mb411 as active low. The sh4 INTC expects active high */

fe5ac8a273268df99334fc368372b25537564243
Pawel Moll <pawel.moll@st.com>
2010-01-19 14:37:46 +0000

    ipidtv7105: Rename board to iptv7105
    This patch changes all references to the IPIDTV7105 board to
    the new, simpler name: IPTV7105.
    
    It also contains fixes in Ethernet setup and default configuration
    required to get the board actually working.
    
    Signed-off-by: Chris Smith <chris.smith@st.com>
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>
    Signed-off-by: Pawel Moll <pawel.moll@st.com>

--- arch/sh/boards/mach-iptv7105/setup.c
+++ arch/sh/boards/mach-iptv7105/setup.c
@@ -1,5 +1,5 @@
 /*
- * arch/sh/boards/mach-ipidtv7105/setup.c
+ * arch/sh/boards/mach-iptv7105/setup.c
  *
  * Copyright (C) 2008 STMicroelectronics Limited
  * Author: Stuart Menefy (stuart.menefy@st.com)
@@ -28,15 +28,15 @@
 #include <linux/spi/flash.h>
 #include <asm/irq-ilc.h>
 
-#define IPIDTV7105_PIO_PHY_RESET stm_gpio(15, 5)
-#define IPIDTV7105_PIO_PCI_IDSEL stm_gpio(10, 2)
-#define IPIDTV7105_PIO_PCI_SERR  stm_gpio(15, 4)
-#define IPIDTV7105_PIO_PCI_RESET stm_gpio(15, 7)
-#define IPIDTV7105_PIO_FLASH_VPP stm_gpio(6, 5)
-#define IPIDTV7105_PIO_SPI_BOOT_CLK   stm_gpio(15, 0)
-#define IPIDTV7105_PIO_SPI_BOOT_DOUT  stm_gpio(15, 1)
-#define IPIDTV7105_PIO_SPI_BOOT_NOTCS stm_gpio(15, 2)
-#define IPIDTV7105_PIO_SPI_BOOT_DIN   stm_gpio(15, 3)
+#define IPTV7105_PIO_PHY_RESET stm_gpio(15, 5)
+#define IPTV7105_PIO_PCI_IDSEL stm_gpio(10, 2)
+#define IPTV7105_PIO_PCI_SERR  stm_gpio(15, 4)
+#define IPTV7105_PIO_PCI_RESET stm_gpio(15, 7)
+#define IPTV7105_PIO_FLASH_VPP stm_gpio(6, 5)
+#define IPTV7105_PIO_SPI_BOOT_CLK   stm_gpio(15, 0)
+#define IPTV7105_PIO_SPI_BOOT_DOUT  stm_gpio(15, 1)
+#define IPTV7105_PIO_SPI_BOOT_NOTCS stm_gpio(15, 2)
+#define IPTV7105_PIO_SPI_BOOT_DIN   stm_gpio(15, 3)
 
 /*
  * Comment out this line to use NAND through the EMI bit-banging driver
@@ -44,9 +44,9 @@
  */
 #define NAND_USES_FLEX
 
-static void __init ipidtv7105_setup(char **cmdline_p)
+static void __init iptv7105_setup(char **cmdline_p)
 {
-	printk("STMicroelectronics STx7105 IPTVPluggin board initialisation\n");
+	printk(KERN_INFO "STMicroelectronics STx7105 IPTV board initialisation\n");
 
 	stx7105_early_device_init();
 
@@ -69,45 +69,26 @@ static void __init ipidtv7105_setup(char **cmdline_p)
 			.is_console = 0, });
 }
 
-static struct platform_device ipidtv7105_leds = {
-	.name = "leds-gpio",
-	.id = 0,
-	.dev.platform_data = &(struct gpio_led_platform_data) {
-		.num_leds = 2,
-		.leds = (struct gpio_led[]) {
-			{
-				.name = "LD5",
-				.default_trigger = "heartbeat",
-				.gpio = stm_gpio(2, 4),
-			},
-			{
-				.name = "LD6",
-				.gpio = stm_gpio(2, 3),
-			},
-		},
-	},
-};
-
-static int ipidtv7105_phy_reset(void *bus)
+static int iptv7105_phy_reset(void *bus)
 {
-	gpio_set_value(IPIDTV7105_PIO_PHY_RESET, 0);
+	gpio_set_value(IPTV7105_PIO_PHY_RESET, 0);
 	udelay(100);
-	gpio_set_value(IPIDTV7105_PIO_PHY_RESET, 1);
+	gpio_set_value(IPTV7105_PIO_PHY_RESET, 1);
 	udelay(1);
 
 	return 1;
 }
 
-static struct stm_plat_stmmacphy_data ipidtv7105_phy_private_data = {
+static struct stm_plat_stmmacphy_data iptv7105_phy_private_data = {
 	/* Micrel KSZ8041FTL */
 	.bus_id = 0,
 	.phy_addr = -1,
 	.phy_mask = 0,
 	.interface = PHY_INTERFACE_MODE_MII,
-	.phy_reset = &ipidtv7105_phy_reset,
+	.phy_reset = &iptv7105_phy_reset,
 };
 
-static struct platform_device ipidtv7105_phy_device = {
+static struct platform_device iptv7105_phy_device = {
 	.name		= "stmmacphy",
 	.id		= 0,
 	.num_resources	= 1,
@@ -120,7 +101,7 @@ static struct platform_device ipidtv7105_phy_device = {
 		},
 	},
 	.dev = {
-		.platform_data = &ipidtv7105_phy_private_data,
+		.platform_data = &iptv7105_phy_private_data,
 	}
 };
 
@@ -199,9 +180,8 @@ static struct platform_device nand_device = {
 };
 #endif
 
-static struct platform_device *ipidtv7105_devices[] __initdata = {
-	&ipidtv7105_leds,
-	&ipidtv7105_phy_device,
+static struct platform_device *iptv7105_devices[] __initdata = {
+	&iptv7105_phy_device,
 #ifndef NAND_USES_FLEX
 	&nand_device,
 #endif
@@ -209,7 +189,7 @@ static struct platform_device *ipidtv7105_devices[] __initdata = {
 
 /* PCI configuration */
 
-static struct stm_plat_pci_config ipidtv7105_pci_config = {
+static struct stm_plat_pci_config iptv7105_pci_config = {
 	.pci_irq = {
 		[0] = PCI_PIN_DEFAULT,
 		[1] = PCI_PIN_DEFAULT,
@@ -226,58 +206,57 @@ static struct stm_plat_pci_config ipidtv7105_pci_config = {
 		[3] = PCI_PIN_UNUSED
 	},
 	.pci_clk = 33333333,
-	.pci_reset_gpio = IPIDTV7105_PIO_PCI_RESET,
+	.pci_reset_gpio = IPTV7105_PIO_PCI_RESET,
 };
 
 int pcibios_map_platform_irq(struct pci_dev *dev, u8 slot, u8 pin)
 {
        /* We can use the standard function on this board */
-       return stx7105_pcibios_map_platform_irq(&ipidtv7105_pci_config, pin);
+       return stx7105_pcibios_map_platform_irq(&iptv7105_pci_config, pin);
 }
 
-static int __init ipidtv7105_devices_init(void)
+static int __init iptv7105_devices_init(void)
 {
 	/* The IDSEL line is connected to PIO10.2 only... Luckily
 	 * there is just one slot, so we can just force 1... */
-	if (gpio_request(IPIDTV7105_PIO_PCI_IDSEL, "PCI_IDSEL") == 0)
-		gpio_direction_output(IPIDTV7105_PIO_PCI_IDSEL, 1);
+	if (gpio_request(IPTV7105_PIO_PCI_IDSEL, "PCI_IDSEL") == 0)
+		gpio_direction_output(IPTV7105_PIO_PCI_IDSEL, 1);
 	else
-		printk(KERN_ERR "ipidtv7105: Failed to claim PCI_IDSEL PIO!\n");
+		printk(KERN_ERR "iptv7105: Failed to claim PCI_IDSEL PIO!\n");
 	/* Setup the PCI_SERR# PIO */
-	if (gpio_request(IPIDTV7105_PIO_PCI_SERR, "PCI_SERR#") == 0) {
-		gpio_direction_input(IPIDTV7105_PIO_PCI_SERR);
+	if (gpio_request(IPTV7105_PIO_PCI_SERR, "PCI_SERR#") == 0) {
+		gpio_direction_input(IPTV7105_PIO_PCI_SERR);
 		iptv7105_pci_config.serr_irq =
-				gpio_to_irq(IPIDTV7105_PIO_PCI_SERR);
-		set_irq_type(ipidtv7105_pci_config.serr_irq,
-				IRQ_TYPE_LEVEL_LOW);
+				gpio_to_irq(IPTV7105_PIO_PCI_SERR);
+		set_irq_type(iptv7105_pci_config.serr_irq, IRQ_TYPE_LEVEL_LOW);
 	} else {
-		printk(KERN_ERR "ipidtv7105: Failed to claim PCI SERR PIO!\n");
+		printk(KERN_ERR "iptv7105: Failed to claim PCI SERR PIO!\n");
 	}
 	/* And finally! */
-	stx7105_configure_pci(&ipidtv7105_pci_config);
+	stx7105_configure_pci(&iptv7105_pci_config);
 
 	stx7105_configure_sata();
 
 	/* Configure the SPI Boot as input */
-	if (gpio_request(IPIDTV7105_PIO_SPI_BOOT_CLK, "SPI Boot CLK") == 0)
-		gpio_direction_input(IPIDTV7105_PIO_SPI_BOOT_CLK);
+	if (gpio_request(IPTV7105_PIO_SPI_BOOT_CLK, "SPI Boot CLK") == 0)
+		gpio_direction_input(IPTV7105_PIO_SPI_BOOT_CLK);
 	else
-		printk(KERN_ERR "ipidtv7105: Failed to claim SPI Boot CLK!\n");
+		printk(KERN_ERR "iptv7105: Failed to claim SPI Boot CLK!\n");
 
-	if (gpio_request(IPIDTV7105_PIO_SPI_BOOT_DOUT, "SPI Boot DOUT") == 0)
-		gpio_direction_input(IPIDTV7105_PIO_SPI_BOOT_DOUT);
+	if (gpio_request(IPTV7105_PIO_SPI_BOOT_DOUT, "SPI Boot DOUT") == 0)
+		gpio_direction_input(IPTV7105_PIO_SPI_BOOT_DOUT);
 	else
-		printk(KERN_ERR "ipidtv7105: Failed to claim SPI Boot DOUT!\n");
+		printk(KERN_ERR "iptv7105: Failed to claim SPI Boot DOUT!\n");
 
-	if (gpio_request(IPIDTV7105_PIO_SPI_BOOT_NOTCS, "SPI Boot NOTCS") == 0)
-		gpio_direction_input(IPIDTV7105_PIO_SPI_BOOT_NOTCS);
+	if (gpio_request(IPTV7105_PIO_SPI_BOOT_NOTCS, "SPI Boot NOTCS") == 0)
+		gpio_direction_input(IPTV7105_PIO_SPI_BOOT_NOTCS);
 	else
-		printk(KERN_ERR "ipidtv7105: Failed to claim SPI Boot NOTCS!\n");
+		printk(KERN_ERR "iptv7105: Failed to claim SPI Boot NOTCS!\n");
 
-	if (gpio_request(IPIDTV7105_PIO_SPI_BOOT_DIN, "SPI Boot DIN") == 0)
-		gpio_direction_input(IPIDTV7105_PIO_SPI_BOOT_DIN);
+	if (gpio_request(IPTV7105_PIO_SPI_BOOT_DIN, "SPI Boot DIN") == 0)
+		gpio_direction_input(IPTV7105_PIO_SPI_BOOT_DIN);
 	else
-		printk(KERN_ERR "ipidtv7105: Failed to claim SPI Boot DIN!\n");
+		printk(KERN_ERR "iptv7105: Failed to claim SPI Boot DIN!\n");
 	stx7105_configure_ssc_spi(1, &(struct stx7105_ssc_config) {
 			.routing.ssc1.sclk = stx7105_ssc1_sclk_pio2_5,
 			.routing.ssc1.mtsr = stx7105_ssc1_mtsr_pio2_6,
@@ -301,12 +280,12 @@ static int __init ipidtv7105_devices_init(void)
 			.routing.usb1.ovrcur = stx7105_usb1_ovrcur_pio4_6,
 			.routing.usb1.pwr = stx7105_usb1_pwr_pio4_7, });
 
-	gpio_request(IPIDTV7105_PIO_PHY_RESET, "eth_phy_reset");
-	gpio_direction_output(IPIDTV7105_PIO_PHY_RESET, 1);
+	gpio_request(IPTV7105_PIO_PHY_RESET, "eth_phy_reset");
+	gpio_direction_output(IPTV7105_PIO_PHY_RESET, 1);
 
 	stx7105_configure_ethernet(&(struct stx7105_ethernet_config) {
 			.mode = stx7105_ethernet_mode_mii,
-			.ext_clk = 1,
+			.ext_clk = 0,
 			.phy_bus = 0, });
 
 	stx7105_configure_lirc(&(struct stx7105_lirc_config) {
@@ -320,8 +299,8 @@ static int __init ipidtv7105_devices_init(void)
 			});
 
 	/* just permanetly enable the flash*/
-	gpio_request(IPIDTV7105_PIO_FLASH_VPP, "eth_phy_reset");
-	gpio_direction_output(IPIDTV7105_PIO_FLASH_VPP, 1);
+	gpio_request(IPTV7105_PIO_FLASH_VPP, "eth_phy_reset");
+	gpio_direction_output(IPTV7105_PIO_FLASH_VPP, 1);
 #ifdef NAND_USES_FLEX
 	stx7105_configure_nand_flex(1, &nand_bank_data, 1);
 #endif
@@ -330,12 +309,12 @@ static int __init ipidtv7105_devices_init(void)
 	spi_register_board_info(spi_serialflash, ARRAY_SIZE(spi_serialflash));
 #endif
 
-	return platform_add_devices(ipidtv7105_devices,
-				    ARRAY_SIZE(ipidtv7105_devices));
+	return platform_add_devices(iptv7105_devices,
+				    ARRAY_SIZE(iptv7105_devices));
 }
-arch_initcall(ipidtv7105_devices_init);
+arch_initcall(iptv7105_devices_init);
 
-static void __iomem *ipidtv7105_ioport_map(unsigned long port,
+static void __iomem *iptv7105_ioport_map(unsigned long port,
 					   unsigned int size)
 {
 	/*
@@ -348,10 +327,10 @@ static void __iomem *ipidtv7105_ioport_map(unsigned long port,
 	return (void __iomem *)CCN_PVR;
 }
 
-struct sh_machine_vector mv_ipidtv7105 __initmv = {
-	.mv_name	= "ipidtv7105",
-	.mv_setup	= ipidtv7105_setup,
+struct sh_machine_vector mv_iptv7105 __initmv = {
+	.mv_name	= "iptv7105",
+	.mv_setup	= iptv7105_setup,
 	.mv_nr_irqs	= NR_IRQS,
-	.mv_ioport_map	= ipidtv7105_ioport_map,
+	.mv_ioport_map	= iptv7105_ioport_map,
 	STM_PCI_IO_MACHINE_VEC
 };

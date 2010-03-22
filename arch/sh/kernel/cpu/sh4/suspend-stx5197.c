3c2ae7d557e6047a263959799fb46e8f8c67006e
Francesco Virlinzi <francesco.virlinzi@st.com>
2010-03-22 15:17:58 +0100

    stm: suspend: Update stx5197 to use new instructions
    Update the suspend support on stx5197 to use the new suspend instruction
    set introduced in an earlier commit.
    
    Signed-off-by: Francesco Virlinzi <francesco.virlinzi@st.com>

--- arch/sh/kernel/cpu/sh4/suspend-stx5197.c
+++ arch/sh/kernel/cpu/sh4/suspend-stx5197.c
@@ -1,8 +1,7 @@
 /*
  * -------------------------------------------------------------------------
- * <linux_root>/arch/sh/kernel/cpu/sh4/suspend-stx5197.c
- * -------------------------------------------------------------------------
  * Copyright (C) 2009  STMicroelectronics
+ * Copyright (C) 2010  STMicroelectronics
  * Author: Francesco M. Virlinzi  <francesco.virlinzi@st.com>
  *
  * May be copied or modified under the terms of the GNU General Public
@@ -11,162 +10,201 @@
  * ------------------------------------------------------------------------- */
 
 #include <linux/init.h>
-#include <linux/suspend.h>
 #include <linux/errno.h>
 #include <linux/time.h>
 #include <linux/delay.h>
 #include <linux/irqflags.h>
-#include <linux/stm/pm.h>
+#include <linux/io.h>
+
+#include <linux/stm/clk.h>
 #include <linux/stm/sysconf.h>
-#include <linux/irq.h>
-#include <asm/system.h>
-#include <asm/io.h>
-#include <asm/pm.h>
+#include <linux/stm/stx5197.h>
+
 #include <asm/irq.h>
 #include <asm/irq-ilc.h>
 
-#include "./soc-stx5197.h"
+#include "stm_suspend.h"
 
-#define _SYS_MON_J                      (0)
-#define _SYS_MON_J_MASK                 (1)
-#define _SYS_CFG_H                      (2)
-#define _SYS_CFG_H_MASK                 (3)
+#define XTAL			30000000
+#define SYS_CLK			0xfdc00000
+
+#define CLK_PLL_CONFIG0(x)	((x * 8) + 0x0)
+#define CLK_PLL_CONFIG1(x)	((x * 8) + 0x4)
+#define  CLK_PLL_CONFIG1_POFF	(1 << 13)
+#define  CLK_PLL_CONFIG1_LOCK	(1 << 15)
+
+#define CLK_MODE_CTRL		0x110
+#define   CLK_MODE_CTRL_NULL	0x0
+#define   CLK_MODE_CTRL_X1	0x1
+#define   CLK_MODE_CTRL_PROG	0x2
+#define   CLK_MODE_CTRL_STDB	0x3
+
+#define CLK_REDUCED_PM_CTRL	0x114
+#define   CLK_REDUCED_ON_XTAL_MEMSTDBY	(1 << 11)
+#define   CLK_REDUCED_ON_XTAL_STDBY	(~(0x22))
+
+#define CLK_LP_MODE_DIS0	0x118
+#define   CLK_LP_MODE_DIS0_VALUE	(0x3 << 11)
+
+#define CLK_LOCK_CFG		0x300
 
 
+#define _CFG_HI_DENSITY		0xfd901000
+#define _CFG_CTRL_H		0x14
+#define _CFG_MONITOR_J		0x3c
+
+#define LMI_BASE		0xFE000000
+#define LMI_CONTROL		0x10
+#define LMI_BURST_REFRESH	2
+#define LMI_SCR_CST		100
 
 /*
  * System Service Finite State Machine
  *	+-------+   +------+    +------+
  *	| reset |-->|  X1  |<-->| Prog |
  *	+-------+   +------+    +------+
- *	    	       /\	   |
- *	    		|	   \/
+ *	    	        ^	   |
+ *	    		|	   V
  *		wakeup	|       +-------+
  *		event	+-------|Standby|
  *			        +-------+
  */
+static struct clk *comms_clk;
+
 
 /* *************************
  * STANDBY INSTRUCTION TABLE
  * *************************
  */
-#ifdef CONFIG_PM_DEBUG
 static unsigned long stx5197_standby_table[] __cacheline_aligned = {
-CLK_POKE(CLK_LOCK_CFG, 0xf0),
-CLK_POKE(CLK_LOCK_CFG, 0x0f), /* UnLock the clocks */
+POKE32(SYS_CLK + CLK_LOCK_CFG, 0xf0),
+POKE32(SYS_CLK + CLK_LOCK_CFG, 0x0f), /* UnLock the clocks */
 
-CLK_POKE(CLK_MODE_CTRL, CLK_MODE_CTRL_X1),
+POKE32(SYS_CLK + CLK_MODE_CTRL, CLK_MODE_CTRL_X1),
 
 /* 1. Move all the clock on OSC */
-CLK_OR_LONG(CLK_REDUCED_PM_CTRL, CLK_REDUCED_ON_XTAL_STDBY),
-CLK_OR_LONG(CLK_PLL_CONFIG1(0), CLK_PLL_CONFIG1_POFF),
-CLK_POKE(CLK_MODE_CTRL, CLK_MODE_CTRL_PROG),
-CLK_POKE(CLK_LOCK_CFG, 0x100), /* Lock the clocks */
-/* END. */
-_END(),
+OR32(SYS_CLK + CLK_REDUCED_PM_CTRL, CLK_REDUCED_ON_XTAL_STDBY),
+OR32(SYS_CLK + CLK_PLL_CONFIG1(0), CLK_PLL_CONFIG1_POFF),
+POKE32(SYS_CLK + CLK_MODE_CTRL, CLK_MODE_CTRL_PROG),
+POKE32(SYS_CLK + CLK_LOCK_CFG, 0x100), /* Lock the clocks */
 
-CLK_POKE(CLK_LOCK_CFG, 0xf0),
-CLK_POKE(CLK_LOCK_CFG, 0x0f), /* UnLock the clocks */
-CLK_POKE(CLK_MODE_CTRL, CLK_MODE_CTRL_X1),
-CLK_AND_LONG(CLK_REDUCED_PM_CTRL, ~CLK_REDUCED_ON_XTAL_STDBY),
-CLK_AND_LONG(CLK_PLL_CONFIG1(0), ~CLK_PLL_CONFIG1_POFF),
-CLK_WHILE_NEQ(CLK_PLL_CONFIG1(0), CLK_PLL_CONFIG1_LOCK, CLK_PLL_CONFIG1_LOCK),
-CLK_POKE(CLK_MODE_CTRL, CLK_MODE_CTRL_PROG),
-CLK_POKE(CLK_LOCK_CFG, 0x100), /* Lock the clocks */
-_DELAY(),
-_DELAY(),
-_DELAY(),
-_END()
+END_MARKER,
+
+POKE32(SYS_CLK + CLK_LOCK_CFG, 0xf0),
+POKE32(SYS_CLK + CLK_LOCK_CFG, 0x0f), /* UnLock the clocks */
+POKE32(SYS_CLK + CLK_MODE_CTRL, CLK_MODE_CTRL_X1),
+UPDATE32(SYS_CLK + CLK_REDUCED_PM_CTRL, ~CLK_REDUCED_ON_XTAL_STDBY, 0),
+UPDATE32(SYS_CLK + CLK_PLL_CONFIG1(0), ~CLK_PLL_CONFIG1_POFF, 0),
+WHILE_NE32(SYS_CLK + CLK_PLL_CONFIG1(0),
+	   CLK_PLL_CONFIG1_LOCK, CLK_PLL_CONFIG1_LOCK),
+POKE32(SYS_CLK + CLK_MODE_CTRL, CLK_MODE_CTRL_PROG),
+POKE32(SYS_CLK + CLK_LOCK_CFG, 0x100), /* Lock the clocks */
+DELAY(1),
+
+END_MARKER
 };
-#endif
 
 /* *********************
  * MEM INSTRUCTION TABLE
  * *********************
  */
-static unsigned long stx5197_mem_table[] __cacheline_aligned = {
-DATA_OR_LONG(_SYS_CFG_H, _SYS_CFG_H_MASK),
-DATA_WHILE_NEQ(_SYS_MON_J, _SYS_MON_J_MASK, _SYS_MON_J_MASK),
 
-CLK_POKE(CLK_LOCK_CFG, 0xf0),
-CLK_POKE(CLK_LOCK_CFG, 0x0f), /* UnLock the clocks */
+static unsigned long stx5197_mem_table[] __cacheline_aligned = {
+POKE32(LMI_BASE + LMI_CONTROL,
+       (0x4 | (LMI_BURST_REFRESH << 4) | (LMI_SCR_CST << 16))),
+OR32(_CFG_HI_DENSITY + _CFG_CTRL_H, (1 << 26)),
+WHILE_NE32(_CFG_HI_DENSITY + _CFG_MONITOR_J, (1 << 24), (1 << 24)),
+
+POKE32(SYS_CLK + CLK_LOCK_CFG, 0xf0),
+POKE32(SYS_CLK + CLK_LOCK_CFG, 0x0f), /* UnLock the clocks */
 
 /* disable PLLs in standby */
-CLK_OR_LONG(CLK_LP_MODE_DIS0, CLK_LP_MODE_DIS0_VALUE),
-CLK_POKE(CLK_MODE_CTRL, CLK_MODE_CTRL_STDB), /* IN STANDBY */
+OR32(SYS_CLK + CLK_LP_MODE_DIS0, CLK_LP_MODE_DIS0_VALUE),
+POKE32(SYS_CLK + CLK_MODE_CTRL, CLK_MODE_CTRL_STDB), /* IN STANDBY */
 
-_END_NO_SLEEP(),
+END_MARKER,
 /*
  * On a wakeup Event the System Service goes directly in X1 mode */
-CLK_AND_LONG(CLK_PLL_CONFIG1(0), ~CLK_PLL_CONFIG1_POFF),
-CLK_AND_LONG(CLK_PLL_CONFIG1(1), ~CLK_PLL_CONFIG1_POFF),
+UPDATE32(SYS_CLK + CLK_PLL_CONFIG1(0), ~CLK_PLL_CONFIG1_POFF, 0),
+UPDATE32(SYS_CLK + CLK_PLL_CONFIG1(1), ~CLK_PLL_CONFIG1_POFF, 0),
 /* Wait PLLs lock */
-CLK_WHILE_NEQ(CLK_PLL_CONFIG1(0), CLK_PLL_CONFIG1_LOCK, CLK_PLL_CONFIG1_LOCK),
-CLK_WHILE_NEQ(CLK_PLL_CONFIG1(1), CLK_PLL_CONFIG1_LOCK, CLK_PLL_CONFIG1_LOCK),
+WHILE_NE32(SYS_CLK + CLK_PLL_CONFIG1(0),
+	   CLK_PLL_CONFIG1_LOCK, CLK_PLL_CONFIG1_LOCK),
+WHILE_NE32(SYS_CLK + CLK_PLL_CONFIG1(1),
+	   CLK_PLL_CONFIG1_LOCK, CLK_PLL_CONFIG1_LOCK),
 
-CLK_POKE(CLK_MODE_CTRL, CLK_MODE_CTRL_PROG),
-CLK_POKE(CLK_LOCK_CFG, 0x100), /* Lock the clocks */
+POKE32(SYS_CLK + CLK_MODE_CTRL, CLK_MODE_CTRL_PROG),
+POKE32(SYS_CLK + CLK_LOCK_CFG, 0x100), /* Lock the clocks */
 
-_DELAY(),
-_DELAY(),
-DATA_AND_NOT_LONG(_SYS_CFG_H, _SYS_CFG_H_MASK),
-DATA_WHILE_EQ(_SYS_MON_J, _SYS_MON_J_MASK, _SYS_MON_J_MASK),
+DELAY(1),
 
-_DELAY(),
-_DELAY(),
-_DELAY(),
-_DELAY(),
+UPDATE32(_CFG_HI_DENSITY + _CFG_CTRL_H, ~(1 << 26), 0),
+WHILE_NE32(_CFG_HI_DENSITY + _CFG_MONITOR_J, (1 << 24), 0),
 
-_END()
+
+DELAY(1),
+POKE32(LMI_BASE + LMI_CONTROL, 0),
+
+DELAY(1),
+END_MARKER
 };
 
-static unsigned long stx5197_wrt_table[8] __cacheline_aligned;
-
-static int stx5197_suspend_prepare(suspend_state_t state)
+static int stx5197_suspend_begin(suspend_state_t state)
 {
+	comms_clk->rate = XTAL;
 	return 0;
 }
 
-static unsigned long stx5197_iomem[2] __cacheline_aligned = {
-		stx5197_wrt_table,
-		SYS_SERV_BASE_ADDR,};
+static int stx5197_suspend_post_enter(suspend_state_t state)
+{
+	comms_clk->rate = comms_clk->parent->rate;
+	return 0;
+}
 
 static int stx5197_evt_to_irq(unsigned long evt)
 {
 	return ((evt < 0x400) ? ilc2irq(evt) : evt2irq(evt));
 }
 
-static struct sh4_suspend_t st40data __cacheline_aligned = {
-	.iobase = stx5197_iomem,
-	.ops.prepare = stx5197_suspend_prepare,
+static struct stm_platform_suspend_t stx5197_suspend __cacheline_aligned = {
+
+	.flags = NO_SLEEP_ON_MEMSTANDBY,
+
+	.ops.begin = stx5197_suspend_begin,
+
 	.evt_to_irq = stx5197_evt_to_irq,
-#ifdef CONFIG_PM_DEBUG
+	.post_enter = stx5197_suspend_post_enter,
+
 	.stby_tbl = (unsigned long)stx5197_standby_table,
 	.stby_size = DIV_ROUND_UP(ARRAY_SIZE(stx5197_standby_table) *
 			sizeof(long), L1_CACHE_BYTES),
-#endif
+
 	.mem_tbl = (unsigned long)stx5197_mem_table,
 	.mem_size = DIV_ROUND_UP(ARRAY_SIZE(stx5197_mem_table) * sizeof(long),
 			L1_CACHE_BYTES),
-	.wrt_tbl = (unsigned long)stx5197_wrt_table,
-	.wrt_size = DIV_ROUND_UP(ARRAY_SIZE(stx5197_wrt_table) * sizeof(long),
-			L1_CACHE_BYTES),
 };
 
-static int __init suspend_platform_setup(void)
+static int __init stx5197_suspend_setup(void)
 {
+	int i;
+	struct sysconf_field *sc[2];
 
-	struct sysconf_field* sc;
+	sc[0] = sysconf_claim(CFG_MONITOR_J, 24, 24, "PM - LMI");
+	sc[1] = sysconf_claim(CFG_CTRL_H, 26, 26, "PM - LMI");
 
-	sc = sysconf_claim(CFG_MONITOR_J, 24, 24, "LMI pwd ack");
-	stx5197_wrt_table[_SYS_MON_J] = (unsigned long)sysconf_address(sc);
-	stx5197_wrt_table[_SYS_MON_J_MASK] = sysconf_mask(sc);
+	for (i = ARRAY_SIZE(sc)-1; i; --i)
+		if (!sc[i])
+			goto error;
 
-	sc = sysconf_claim(CFG_CTRL_H, 26, 26, "LMI pwd req");
-	stx5197_wrt_table[_SYS_CFG_H] = (unsigned long)sysconf_address(sc);
-	stx5197_wrt_table[_SYS_CFG_H_MASK] = sysconf_mask(sc);
+	comms_clk = clk_get(NULL, "comms_clk");
 
-	return sh4_suspend_register(&st40data);
+	return stm_suspend_register(&stx5197_suspend);
+
+error:
+	for (i = 6; i; --i)
+		if (sc[i])
+			sysconf_release(sc[i]);
+	return -EINVAL;
 }
 
-late_initcall(suspend_platform_setup);
+module_init(stx5197_suspend_setup);

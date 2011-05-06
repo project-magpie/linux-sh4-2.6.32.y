0f88922749616babf00a6006c1245effc1e25761
Stuart Menefy <stuart.menefy@st.com>
2011-05-06 19:08:16 +0100

    stm_sata: Force MB837 with 7108 cut2.0 to use jtag interface0 for sata0
    This patch forces MB837 board with stx7108 cut2.0 to use TAP
    interface to configure SATA. *ONLY* SATA0 is supported for this
    combination, As MIPHY1 is not powered up on this board.
    
    Originally missed in a489a54ecd7bbdc327a8afb0ff1ef0a6ae118167 commit and
    raised in bugzilla#11249
    
    Original patch from Srinivas Kandagatla <srinivas.kandagatla@st.com>
    rewritten to match updated MiPHY and SATA configuration parameters.
    
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>

--- arch/sh/boards/mach-mb837/setup.c
+++ arch/sh/boards/mach-mb837/setup.c
@@ -224,12 +224,21 @@ static int __init mb837_devices_init(void)
 	/* J53C & J53D fitted */
 	stx7108_configure_usb(2);
 
-	stx7108_configure_miphy(&(struct stx7108_miphy_config) {
-			.modes = (enum miphy_mode[2]) {
-				SATA_MODE, SATA_MODE },
-			});
-	stx7108_configure_sata(0, &(struct stx7108_sata_config) { });
-	stx7108_configure_sata(1, &(struct stx7108_sata_config) { });
+	if (cpu_data->cut_major >= 2) {
+		stx7108_configure_miphy(&(struct stx7108_miphy_config) {
+				.force_jtag = 1,
+				.modes = (enum miphy_mode[2]) {
+					SATA_MODE, UNUSED_MODE },
+				});
+		stx7108_configure_sata(0, &(struct stx7108_sata_config) { });
+	} else {
+		stx7108_configure_miphy(&(struct stx7108_miphy_config) {
+				.modes = (enum miphy_mode[2]) {
+					SATA_MODE, SATA_MODE },
+				});
+		stx7108_configure_sata(0, &(struct stx7108_sata_config) { });
+		stx7108_configure_sata(1, &(struct stx7108_sata_config) { });
+	}
 
 #ifdef CONFIG_SH_ST_MB837_STMMAC0
 	stx7108_configure_ethernet(0, &(struct stx7108_ethernet_config) {

70e70e1ea965bb5a73fe311b3bacc0b632fdcf74
Francesco VIRLINZI <francesco.virlinzi@st.com>
2011-05-10 14:54:05 +0200

    rtc_stm_lpc: Fixed check on existing clk.
    This patch adds a check to be sure the lpc_clk is present.
    Without the check the stx7200 doesn't boot at all because
    the LPC_CLK is based on the clock_gen_B clocks which are
    (currently) not supported on stx7200.
    
    Signed-off-by: Francesco Virlinzi <francesco.virlinzi@st.com>
    Reviewed-by: Carmelo Amoroso <carmelo.amoroso@st.com>

--- drivers/rtc/rtc-stm-lpc.c
+++ drivers/rtc/rtc-stm-lpc.c
@@ -340,6 +340,10 @@ static int __devinit stm_rtc_probe(struct platform_device *pdev)
 	}
 
 	rtc->clk = clk_get(&pdev->dev, plat_data->clk_id);
+	if (!rtc->clk) {
+		pr_err("clk %s not found\n", plat_data->clk_id);
+		return -ENODEV;
+	}
 	clk_enable(rtc->clk);
 
 	pr_debug("%s: is using clk: %s @ %lu\n",

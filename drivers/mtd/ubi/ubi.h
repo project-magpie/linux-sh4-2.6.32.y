a34a499dd73bf07e0739e457a7ffb97840db434d
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2011-05-18 16:38:06 +0300

    UBI: make it possible to use struct ubi_device in debug.h
    Current layout does not allow us to add inline functions to debug.h which use
    the 'struct ubi_device' object, because it is undefined there. Move
    '#include "debug.h"' in "ubi.h" down so to make 'struct ubi_device" be defined.
    Additionally, this makes it possible to remove a bunch of forward declarations
    in "debug.h". This is a preparation to the next patch.
    
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- drivers/mtd/ubi/ubi.h
+++ drivers/mtd/ubi/ubi.h
@@ -43,7 +43,6 @@
 
 #include "ubi-media.h"
 #include "scan.h"
-#include "debug.h"
 
 /* Maximum number of supported UBI devices */
 #define UBI_MAX_DEVICES 32
@@ -476,6 +475,8 @@ struct ubi_device {
 
 	struct ubi_debug_info *dbg;
 };
+
+#include "debug.h"
 
 extern struct kmem_cache *ubi_wl_entry_slab;
 extern const struct file_operations ubi_ctrl_cdev_operations;

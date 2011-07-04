0fdc161e06b7632443eb249e842d7ca372fd1bca
Francesco VIRLINZI <francesco.virlinzi@st.com>
2011-07-04 09:56:08 +0200

    stm_hom_stx7108: Updated poketable [v2]
    This patch updates the HoM-7108 poketable to:
    
    1. Put into self-refresh LMI_1 as well as LMI_0. Originally it was
       planned that this should be turned-off ... now it isn't.  This is
       required to support 'lmi_contig=1'.
    
    2. Enable the retention_mode gpio on the DRAM. At the beginning it was
       planned this would be done by the external micro-controller, now
       it's done in Linux because the micro-controller firmware is still
       under development/testing.
    
    3. Do several updates to decrease the power consumption for both the
       DRAM_Phys (now the DRAM subsystem power consumption is ~30-70mW).
    
    In addition the original SDA/SCL are restored in the .complete
    function to support i2c mode after resume.
    
    Signed-off-by: Francesco Virlinzi <francesco.virlinzi@st.com>
    Reviewed-by: Carmelo Amoroso <carmelo.amoroso@st.com>

--- arch/sh/kernel/cpu/sh4/hom-stx7108.c
+++ arch/sh/kernel/cpu/sh4/hom-stx7108.c
@@ -23,6 +23,7 @@
 #include <linux/stm/platform.h>
 #include <linux/stm/gpio.h>
 
+#include <asm/cacheflush.h>
 #include <asm/irq-ilc.h>
 
 #include "stm_hom.h"
@@ -35,6 +36,7 @@
  *  Databook - Version 2.10a - February 4, 2009'
  */
 #define DDR3SS0_REG		0xFDE50000
+#define DDR3SS1_REG		0xFDE70000
 
 #define DDR_SCTL		0x4
 #define  DDR_SCTL_CFG			0x1
@@ -47,11 +49,37 @@
 #define  DDR_STAT_ACCESS		0x3
 #define  DDR_STAT_LOW_POWER		0x5
 
-#define DDR_PHY_IOCRV1		0x31C
-#define DDR_PHY_PIR		0x404
-#define DDR_PHY_DXCCR		0x434
+#define DDR_DTU_CFG		0x208
+# define DDR_DTU_CFG_ENABLE		0x1
 
-#define PCLK	100000000
+/*
+ * The Stx7108 uses the Synopsys IP Dram Phy Controller
+ * For registers description see:
+ * 'DesignWare Cores DDR3/2 SDRAM PHY -
+ *  Databook - February 5, 2009'
+ *
+ * - Table 5.1: PHY Control Register Mapping
+ * and
+ * - Table 5.30: PUB Control Register Mapping
+ */
+#define DDR_PHY_REG(idx)		(0x400 + (idx) * 4)
+
+#define DDR_PHY_PIR			DDR_PHY_REG(1)		/* 0x04 */
+# define DDR_PHY_PIR_PLL_RESET			(1 << 7)
+# define DDR_PHY_PIR_PLL_PD			(1 << 8)
+
+#define DDR_PHY_PGCR0			DDR_PHY_REG(2)		/* 0x08 */
+#define DDR_PHY_PGCR1			DDR_PHY_REG(3)		/* 0x0c */
+
+#define DDR_PHY_ACIOCR			DDR_PHY_REG(12)		/* 0x30 */
+# define DDR_PHY_ACIOCR_OUTPUT_ENABLE		(1 << 1)
+# define DDR_PHY_ACIOCR_PDD			(1 << 3)
+# define DDR_PHY_ACIOCR_PDR			(1 << 4)
+
+#define DDR_PHY_DXCCR			DDR_PHY_REG(13)		/* 0x34 */
+# define DDR_PHY_DXCCR_PDR			(1 << 4)
+
+#define PCLK				100000000
 #define BAUDRATE_VAL_M1(bps)    	\
 	((((bps * (1 << 14)) + (1 << 13)) / (PCLK / (1 << 6))))
 
@@ -66,30 +94,65 @@
 #define GPIO_SET_OFFSET		0x4 /* to push high */
 #define GPIO_RESET_OFFSET	0x8 /* to push low */
 
+#define GPIO_26			0xfe721000
+/*
+ * In the SysBank 2 the alternate function has
+ * to be configured
+ */
 #define SYS_BANK_2		0xfda50000
-#define SYS_PIO_5		0x14
+#define SYS2_GPIO_ALT_FN(_x)	((_x) * 0x4)
 
 #define SYS_BANK_1             0xfde20000
 #define SYS_B1_CFG4            0x4C
 
 
+#define ddr_in_low_power(_ddr_base)				\
+/* Enable DTU */						\
+OR32((_ddr_base) + DDR_DTU_CFG, DDR_DTU_CFG_ENABLE),		\
+								\
+/* 1. Enables the DDR self refresh mode based */		\
+/*    on paraghaph. 7.1.4 -> from ACCESS to LowPower */		\
+								\
+POKE32((_ddr_base)  + DDR_SCTL, DDR_SCTL_SLEEP),		\
+WHILE_NE32((_ddr_base) + DDR_STAT, DDR_STAT_LOW_POWER, DDR_STAT_LOW_POWER),\
+								\
+/* 2. Turn in LowPower the DDR-Phy */				\
+OR32((_ddr_base) + DDR_PHY_PIR, DDR_PHY_PIR_PLL_RESET),		\
+OR32((_ddr_base) + DDR_PHY_PIR, DDR_PHY_PIR_PLL_PD),		\
+								\
+POKE32((_ddr_base) + DDR_PHY_ACIOCR, -1),			\
+UPDATE32((_ddr_base) + DDR_PHY_ACIOCR, ~1, 0),			\
+								\
+OR32((_ddr_base) + DDR_PHY_DXCCR, DDR_PHY_DXCCR_PDR),		\
+								\
+/* Disable CK going to the SDRAM */				\
+UPDATE32((_ddr_base) + DDR_PHY_PGCR0, ~(0x3f << 26), 0),	\
+UPDATE32((_ddr_base) + DDR_PHY_PGCR1, ~(5 << 12), 0)
+
+
 static unsigned long stx7108_hom_table[] __cacheline_aligned = {
-/* 1. Enables the DDR self refresh mode based on paraghaph. 7.1.4
- *    -> from ACCESS to LowPower
+ddr_in_low_power(DDR3SS0_REG),
+ddr_in_low_power(DDR3SS1_REG),
+/*
+ * Enable retention mode gpio[26][4]
+ *
+ * It was intended that this would be done by the external uMC
+ * but it is now done here.
+ *
+ * set gpio 26.4 low.
+ * - Also if the system is setting low all the gpio 26 lines
+ *   it isn't a problem because the system is going to shutdown
  */
-POKE32(DDR3SS0_REG + DDR_SCTL, DDR_SCTL_SLEEP),
-WHILE_NE32(DDR3SS0_REG + DDR_STAT, DDR_STAT_LOW_POWER, DDR_STAT_LOW_POWER),
-
-OR32(DDR3SS0_REG + DDR_PHY_DXCCR, 1),
-
-OR32(DDR3SS0_REG + DDR_PHY_PIR, 1 << 7),
+POKE32(0xfe721000 + STM_GPIO_REG_CLR_POUT, -1),
 
 /*
+ * Notify the 'ready for power_off to the uMC
+ *
  * Force the SCL/SDA lines low to guarantee the i2c violation
  */
 OR32(SYS_BANK_1 + SYS_B1_CFG4, 1),
-UPDATE32(SYS_BANK_2 + SYS_PIO_5, ~(3 << 24), 0),
-UPDATE32(SYS_BANK_2 + SYS_PIO_5, ~(3 << 28), 0),
+UPDATE32(SYS_BANK_2 + SYS2_GPIO_ALT_FN(5), ~(3 << 24), 0),
+UPDATE32(SYS_BANK_2 + SYS2_GPIO_ALT_FN(5), ~(3 << 28), 0),
 POKE32(GPIO(5) + STM_GPIO_REG_CLR_POUT, 0x3 << 6),
 
 /* END. */
@@ -126,13 +189,23 @@ static int stx7108_hom_prepare(void)
 
 static int stx7108_hom_complete(void)
 {
-	hom_printk("%s - Enter ", __func__);
+
+	/* set again the retention mode */
+	gpio_direction_output(stm_gpio(26, 4), 1);
+
 	/* Enable the INTC2 */
 	writel(7, 0xfda30000 + 0x00);	/* INTPRI00 */
 	writel(1, 0xfda30000 + 0x60);	/* INTMSKCLR00 */
 
 	stm_defrost_board(NULL);
-	hom_printk("%s - DONE ", __func__);
+
+	/*
+	 * Set SCA/SCL again as STM_GPIO_DIRECTION_BIDIR to be
+	 * working as I2C-bus
+	 */
+	stm_gpio_direction(stm_gpio(5, 6), STM_GPIO_DIRECTION_BIDIR);
+	stm_gpio_direction(stm_gpio(5, 7), STM_GPIO_DIRECTION_BIDIR);
+
 	return 0;
 }
 
@@ -149,6 +222,20 @@ static struct stm_mem_hibernation stx7108_hom = {
 static int __init hom_stx7108_setup(void)
 {
 	int ret;
+
+	ret = gpio_request(stm_gpio(26, 4), "LMI retention mode");
+	if (ret) {
+		pr_err("[STM]: [PM]: [HoM]: GPIO for retention mode"
+			"not acquired\n");
+		return ret;
+	}
+
+	/*
+	 * set the gpio_retention_mode high
+	 * before it's configured as 'STM_GPIO_DIRECTION_OUT'
+	 * to avoid it's go low because not-initialized
+	 */
+	gpio_direction_output(stm_gpio(26, 4), 1);
 
 	ret = stm_hom_register(&stx7108_hom);
 	if (!ret) {

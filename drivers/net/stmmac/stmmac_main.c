52fa385eedb40573a1924c967db416c83abfe46a
Giuseppe Cavallaro <peppe.cavallaro@st.com>
2011-08-22 16:07:19 +0200

    stmmac: remove the mmc code
    DWMAC Management Counters (MMC) are not fully support.
    The minimal support added in the past allowed to
    only disable counters (if present) and mask their
    interrupts.
    This patch prepares the driver to support the MMC
    removing obsolete code.
    
    Signed-off-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>

--- drivers/net/stmmac/stmmac_main.c
+++ drivers/net/stmmac/stmmac_main.c
@@ -827,10 +827,6 @@ static int stmmac_open(struct net_device *dev)
 	if (priv->plat->tx_coe)
 		pr_info("\tTX Checksum insertion supported\n");
 
-	/* Initialise the MMC (if present) to disable all interrupts. */
-	writel(0xffffffff, priv->ioaddr + MMC_HIGH_INTR_MASK);
-	writel(0xffffffff, priv->ioaddr + MMC_LOW_INTR_MASK);
-
 	/* Request the IRQ lines */
 	ret = request_irq(dev->irq, stmmac_interrupt,
 			 IRQF_SHARED, dev->name, dev);

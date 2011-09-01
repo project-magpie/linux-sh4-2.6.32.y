fac2061b7b5c1bd6f263a971a72b1f34c49a4215
Giuseppe Cavallaro <peppe.cavallaro@st.com>
2011-09-01 08:22:51 +0200

    stmmac: protect tx process with lock
    This patch fixes a problem raised on Orly SMP platform
    where, in case of fragmented frames, the descriptors
    in the ring resulted broken.
    
    Signed-off-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>
    Tested-by: Srinivas Kandagatla <srinivas.kandagatla@st.com>

--- drivers/net/stmmac/stmmac_main.c
+++ drivers/net/stmmac/stmmac_main.c
@@ -620,6 +620,8 @@ static void stmmac_tx(struct stmmac_priv *priv)
 {
 	unsigned int txsize = priv->dma_tx_size;
 
+	spin_lock(&priv->tx_lock);
+
 	while (priv->dirty_tx != priv->cur_tx) {
 		int last;
 		unsigned int entry = priv->dirty_tx % txsize;
@@ -685,6 +687,7 @@ static void stmmac_tx(struct stmmac_priv *priv)
 		}
 		netif_tx_unlock(priv->dev);
 	}
+	spin_unlock(&priv->tx_lock);
 }
 
 static inline void stmmac_enable_irq(struct stmmac_priv *priv)
@@ -1215,6 +1218,8 @@ static netdev_tx_t stmmac_xmit(struct sk_buff *skb, struct net_device *dev)
 	if (unlikely(skb_is_gso(skb)))
 		return stmmac_sw_tso(priv, skb);
 
+	spin_lock(&priv->tx_lock);
+
 	if (likely((skb->ip_summed == CHECKSUM_PARTIAL))) {
 		if (unlikely((!priv->plat->tx_coe) ||
 			     (priv->no_csum_insertion)))
@@ -1304,6 +1309,8 @@ static netdev_tx_t stmmac_xmit(struct sk_buff *skb, struct net_device *dev)
 	dev->stats.tx_bytes += skb->len;
 
 	priv->hw->dma->enable_dma_transmission(priv->ioaddr);
+
+	spin_unlock(&priv->tx_lock);
 
 	return NETDEV_TX_OK;
 }
@@ -1880,6 +1887,7 @@ static int stmmac_probe(struct net_device *dev)
 			"please, use ifconfig or nwhwconfig!\n");
 
 	spin_lock_init(&priv->lock);
+	spin_lock_init(&priv->tx_lock);
 
 	ret = register_netdev(dev);
 	if (ret) {

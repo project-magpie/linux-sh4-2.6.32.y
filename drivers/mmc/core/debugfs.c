add104e77c30996313b0dce30172afd17a60052b
Giuseppe Cavallaro <peppe.cavallaro@st.com>
2011-10-04 11:21:26 +0200

    mmc: debugfs: parse ext_csd via debug_fs
    This patch enhances the debug information reported
    for the mmc in /sys/kernel/debug/mmc.
    It adds a new function to parse the extended CSD registers.
    Before this patch, we can only the a big buffer of numbers
    that had to be parsed off line according to the JEDEC SPEC.
    
    Signed-off-by: Youssef TRIKI <youssef.triki@st.com>
    Signed-off-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>

--- drivers/mmc/core/debugfs.c
+++ drivers/mmc/core/debugfs.c
@@ -216,19 +216,11 @@ static int mmc_dbg_card_status_get(void *data, u64 *val)
 DEFINE_SIMPLE_ATTRIBUTE(mmc_dbg_card_status_fops, mmc_dbg_card_status_get,
 		NULL, "%08llx\n");
 
-#define EXT_CSD_STR_LEN 1025
-
-static int mmc_ext_csd_open(struct inode *inode, struct file *filp)
+static int mmc_ext_csd_read(struct seq_file *s, void *data)
 {
-	struct mmc_card *card = inode->i_private;
-	char *buf;
-	ssize_t n = 0;
+	struct mmc_card *card = s->private;
 	u8 *ext_csd;
-	int err, i;
-
-	buf = kmalloc(EXT_CSD_STR_LEN + 1, GFP_KERNEL);
-	if (!buf)
-		return -ENOMEM;
+	int err;
 
 	ext_csd = kmalloc(512, GFP_KERNEL);
 	if (!ext_csd) {
@@ -236,47 +228,108 @@ static int mmc_ext_csd_open(struct inode *inode, struct file *filp)
 		goto out_free;
 	}
 
+	/* Get extended CSD */
 	mmc_claim_host(card->host);
 	err = mmc_send_ext_csd(card, ext_csd);
 	mmc_release_host(card->host);
 	if (err)
 		goto out_free;
 
-	for (i = 511; i >= 0; i--)
-		n += sprintf(buf + n, "%02x", ext_csd[i]);
-	n += sprintf(buf + n, "\n");
-	BUG_ON(n != EXT_CSD_STR_LEN);
-
-	filp->private_data = buf;
-	kfree(ext_csd);
-	return 0;
+	/* aligned to the 4.41 */
+	seq_printf(s, "s_cmd_set: 0x%02x\n", ext_csd[504]);
+	seq_printf(s, "ini_timeout_ap: 0x%02x\n", ext_csd[241]);
+	seq_printf(s, "pwr_cl_ddr_52_360 0x%02x\n", ext_csd[239]);
+	seq_printf(s, "pwr_cl_ddr_52_195 0x%02x\n", ext_csd[238]);
+	seq_printf(s, "min_perf_ddr_w_8_52 0x%02x\n", ext_csd[235]);
+	seq_printf(s, "min_perf_ddr_r_8_52 0x%02x\n", ext_csd[234]);
+	seq_printf(s, "trim_mult  0x%02x\n", ext_csd[232]);
+	seq_printf(s, "sec_feature_support 0x%02x\n", ext_csd[231]);
+	seq_printf(s, "sec_erase_mult 0x%02x\n", ext_csd[230]);
+	seq_printf(s, "sec_trim_mult  0x%02x\n", ext_csd[229]);
+	seq_printf(s, "boot_info 0x%02x\n", ext_csd[228]);
+	seq_printf(s, "boot_size_multi 0x%02x\n", ext_csd[226]);
+	seq_printf(s, "acc_size 0x%02x\n", ext_csd[225]);
+	seq_printf(s, "hc_erase_grp_size 0x%02x\n", ext_csd[224]);
+	seq_printf(s, "erase_timeout_mult 0x%02x\n", ext_csd[223]);
+	seq_printf(s, "rel_wr_sec_c 0x%02x\n", ext_csd[222]);
+	seq_printf(s, "hc_wp_grp_size 0x%02x\n", ext_csd[221]);
+	seq_printf(s, "s_c_vcc 0x%02x\n", ext_csd[220]);
+	seq_printf(s, "s_c_vccq 0x%02x\n", ext_csd[219]);
+	seq_printf(s, "s_a_timeout 0x%02x\n", ext_csd[217]);
+	seq_printf(s, "sec_count 0x%02x\n", (ext_csd[215] << 24) |
+		      (ext_csd[214] << 16) | (ext_csd[213] << 8)  |
+		      ext_csd[212]);
+	seq_printf(s, "min_perf_w_8_52 0x%02x\n", ext_csd[210]);
+	seq_printf(s, "min_perf_r_8_52 0x%02x\n", ext_csd[209]);
+	seq_printf(s, "min_perf_w_8_26_4_52 0x%02x\n", ext_csd[208]);
+	seq_printf(s, "min_perf_r_8_26_4_52 0x%02x\n", ext_csd[207]);
+	seq_printf(s, "min_perf_w_4_26 0x%02x\n", ext_csd[206]);
+	seq_printf(s, "min_perf_r_4_26 0x%02x\n", ext_csd[205]);
+	seq_printf(s, "pwr_cl_26_360 0x%02x\n", ext_csd[203]);
+	seq_printf(s, "pwr_cl_52_360 0x%02x\n", ext_csd[202]);
+	seq_printf(s, "pwr_cl_26_195 0x%02x\n", ext_csd[201]);
+	seq_printf(s, "pwr_cl_52_195 0x%02x\n", ext_csd[200]);
+	seq_printf(s, "card_type  0x%02x\n", ext_csd[196]);
+	seq_printf(s, "csd_structure 0x%02x\n", ext_csd[194]);
+	seq_printf(s, "ext_csd_rev 0x%02x\n", ext_csd[192]);
+	seq_printf(s, "cmd_set 0x%02x\n", ext_csd[191]);
+	seq_printf(s, "cmd_set_rev 0x%02x\n", ext_csd[189]);
+	seq_printf(s, "power_class 0x%02x\n", ext_csd[187]);
+	seq_printf(s, "hs_timing 0x%02x\n", ext_csd[185]);
+	seq_printf(s, "bus_width 0x%02x\n", ext_csd[183]);
+	seq_printf(s, "erased_mem_cont 0x%02x\n", ext_csd[181]);
+	seq_printf(s, "partition_config 0x%02x\n", ext_csd[179]);
+	seq_printf(s, "boot_config_prot 0x%02x\n", ext_csd[178]);
+	seq_printf(s, "boot_bus_width 0x%02x\n", ext_csd[177]);
+	seq_printf(s, "erase_group_def 0x%02x\n", ext_csd[175]);
+	seq_printf(s, "boot_wp 0x%02x\n", ext_csd[173]);
+	seq_printf(s, "user_wp 0x%02x\n", ext_csd[171]);
+	seq_printf(s, "fw_config 0x%02x\n", ext_csd[169]);
+	seq_printf(s, "rpmb_size_mult 0x%02x\n", ext_csd[168]);
+	seq_printf(s, "rst_n_function 0x%02x\n", ext_csd[162]);
+	seq_printf(s, "partitioning_support 0x%02x\n", ext_csd[160]);
+	seq_printf(s, "max_enh_size_mult[2] 0x%02x\n", ext_csd[159]);
+	seq_printf(s, "max_enh_size_mult[1] 0x%02x\n", ext_csd[158]);
+	seq_printf(s, "max_enh_size_mult[0] 0x%02x\n", ext_csd[157]);
+	seq_printf(s, "partitions_attribute 0x%02x\n", ext_csd[156]);
+	seq_printf(s, "partition_setting_completed 0x%02x\n",
+		      ext_csd[155]);
+	seq_printf(s, "gp_size_mult_4[2] 0x%02x\n", ext_csd[154]);
+	seq_printf(s, "gp_size_mult_4[1] 0x%02x\n", ext_csd[153]);
+	seq_printf(s, "gp_size_mult_4[0] 0x%02x\n", ext_csd[152]);
+	seq_printf(s, "gp_size_mult_3[2] 0x%02x\n", ext_csd[151]);
+	seq_printf(s, "gp_size_mult_3[1] 0x%02x\n", ext_csd[150]);
+	seq_printf(s, "gp_size_mult_3[0] 0x%02x\n", ext_csd[149]);
+	seq_printf(s, "gp_size_mult_2[2] 0x%02x\n", ext_csd[148]);
+	seq_printf(s, "gp_size_mult_2[1] 0x%02x\n", ext_csd[147]);
+	seq_printf(s, "gp_size_mult_2[0] 0x%02x\n", ext_csd[146]);
+	seq_printf(s, "gp_size_mult_1[2] 0x%02x\n", ext_csd[145]);
+	seq_printf(s, "gp_size_mult_1[1] 0x%02x\n", ext_csd[144]);
+	seq_printf(s, "gp_size_mult_1[0] 0x%02x\n", ext_csd[143]);
+	seq_printf(s, "enh_size_mult[2] 0x%02x\n", ext_csd[142]);
+	seq_printf(s, "enh_size_mult[1] 0x%02x\n", ext_csd[141]);
+	seq_printf(s, "enh_size_mult[0] 0x%02x\n", ext_csd[140]);
+	seq_printf(s, "enh_start_addr[3] 0x%02x\n", ext_csd[139]);
+	seq_printf(s, "enh_start_addr[2] 0x%02x\n", ext_csd[138]);
+	seq_printf(s, "enh_start_addr[1] 0x%02x\n", ext_csd[137]);
+	seq_printf(s, "enh_start_addr[0] 0x%02x\n", ext_csd[136]);
+	seq_printf(s, "sec_bad_blk_mgmnt 0x%02x\n", ext_csd[134]);
 
 out_free:
-	kfree(buf);
 	kfree(ext_csd);
 	return err;
 }
 
-static ssize_t mmc_ext_csd_read(struct file *filp, char __user *ubuf,
-				size_t cnt, loff_t *ppos)
+static int mmc_ext_csd_open(struct inode *inode, struct file *file)
 {
-	char *buf = filp->private_data;
-
-	return simple_read_from_buffer(ubuf, cnt, ppos,
-				       buf, EXT_CSD_STR_LEN);
-}
-
-static int mmc_ext_csd_release(struct inode *inode, struct file *file)
-{
-	kfree(file->private_data);
-	return 0;
+	return single_open(file, mmc_ext_csd_read, inode->i_private);
 }
 
 static const struct file_operations mmc_dbg_ext_csd_fops = {
 	.open		= mmc_ext_csd_open,
-	.read		= mmc_ext_csd_read,
-	.release	= mmc_ext_csd_release,
-	.llseek		= default_llseek,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
 };
 
 void mmc_add_card_debugfs(struct mmc_card *card)

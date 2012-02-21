9f1c52f6231769139a143310c5fbb3a9578fadbe
Stuart Menefy <stuart.menefy@st.com>
2012-02-21 20:05:36 +0000

    stm: pio-control: Switch to generic retiming macros
    Despite the flexibility of the STx7108 style retiming registers, there
    are only a limited number of settings which actually make sense. So
    provide macro definitions of the appropriate settings, and use those
    directly.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@st.com>
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>

--- include/linux/stm/pio-control.h
+++ include/linux/stm/pio-control.h
@@ -28,4 +28,87 @@ struct stm_pio_control_retime_config {
 	int delay_input:3;
 };
 
+/*
+ * 	Generic Retime Padlogic possible modes
+ * Refer to GRP Functional specs (ADCS 8198257) for more details
+ */
+
+/*
+ * B Mode
+ * Bypass retime with optional delay
+ */
+#define RET_BYPASS(delay) (&(struct stm_pio_control_retime_config){ \
+	.retime = 0, \
+	.clk1notclk0 = -1, \
+	.clknotdata = 0, \
+	.double_edge = -1, \
+	.invertclk = -1, \
+	.delay_input = delay, \
+})
+
+/*
+ * R0, R1, R0D, R1D modes
+ * single-edge data non inverted clock, retime data with clk
+ */
+#define RET_SE_NICLK_IO(delay, clk) (&(struct stm_pio_control_retime_config){ \
+	.retime = 1, \
+	.clk1notclk0 = clk, \
+	.clknotdata = 0, \
+	.double_edge = 0, \
+	.invertclk = 0, \
+	.delay_input = delay, \
+})
+
+/*
+ * RIV0, RIV1, RIV0D, RIV1D modes
+ * single-edge data inverted clock, retime data with clk
+ */
+#define RET_SE_ICLK_IO(delay, clk) (&(struct stm_pio_control_retime_config){ \
+	.retime = 1, \
+	.clk1notclk0 = clk, \
+	.clknotdata = 0, \
+	.double_edge = 0, \
+	.invertclk = 1, \
+	.delay_input = delay, \
+})
+
+/*
+ * R0E, R1E, R0ED, R1ED modes
+ * double-edge data, retime data with clk
+ */
+#define RET_DE_IO(delay, clk) (&(struct stm_pio_control_retime_config){ \
+	.retime = 1, \
+	.clk1notclk0 = clk, \
+	.clknotdata = 0, \
+	.double_edge = 1, \
+	.invertclk = -1, \
+	.delay_input = delay, \
+})
+
+/*
+ * CIV0, CIV1 modes with inverted clock
+ * Retiming the clk pins will park clock & reduce the noise within the core.
+ */
+#define RET_ICLK(clk) (&(struct stm_pio_control_retime_config){ \
+	.retime = 1, \
+	.clk1notclk0 = clk, \
+	.clknotdata = 1, \
+	.double_edge = -1, \
+	.invertclk = 1, \
+	.delay_input = 0, \
+})
+
+/*
+ * CLK0, CLK1 modes with non-inverted clock
+ * Retiming the clk pins will park clock & reduce the noise within the core.
+ */
+#define RET_NICLK(clk) (&(struct stm_pio_control_retime_config){ \
+	.retime = 1, \
+	.clk1notclk0 = clk, \
+	.clknotdata = 1, \
+	.double_edge = -1, \
+	.invertclk = 0, \
+	.delay_input = 0, \
+})
+
 #endif

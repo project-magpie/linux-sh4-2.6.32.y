5d97a14ffbe066ed08d74583df4ac376f15ca3dc
Chris Smith <chris.smith@st.com>
2012-02-22 08:50:40 +0000

    mm: Allow a default value for min_free_kbytes to be set using Kconfig
    The default value of min_free_kbytes is a function of the size of system
    memory, and it can then be changed using procfs. This patch allows a
    different default value to be specified using Kconfig, which is useful
    for users who require a different figure but do not have procfs mounted.
    
    Signed-off-by: Chris Smith <chris.smith@st.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4712,9 +4712,15 @@ static void __init setup_per_zone_inactive_ratio(void)
  * 4096MB:	8192k
  * 8192MB:	11584k
  * 16384MB:	16384k
+ *
+ * It can also be set from Kconfig, useful for users who don't have /proc
+ * mounted.
  */
 static int __init init_per_zone_wmark_min(void)
 {
+#ifdef CONFIG_MIN_FREE_KBYTES_VAL
+	min_free_kbytes = CONFIG_MIN_FREE_KBYTES_VAL;
+#else
 	unsigned long lowmem_kbytes;
 
 	lowmem_kbytes = nr_free_buffer_pages() * (PAGE_SIZE >> 10);
@@ -4724,6 +4730,7 @@ static int __init init_per_zone_wmark_min(void)
 		min_free_kbytes = 128;
 	if (min_free_kbytes > 65536)
 		min_free_kbytes = 65536;
+#endif
 	setup_per_zone_wmarks();
 	setup_per_zone_lowmem_reserve();
 	setup_per_zone_inactive_ratio();

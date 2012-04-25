bf0cd4135c8c6770630095b6f4e935cb11df0f7b
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2012-04-25 09:15:38 +0300

    UBI: always warn if case of I/O errors
    Currently UBI silently retries I/O operation in case of errors. This patch
    makes it emit a warning before retrying. This should allow users notice issues
    earlier.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- drivers/mtd/ubi/io.c
+++ drivers/mtd/ubi/io.c
@@ -188,9 +188,9 @@ retry:
 		}
 
 		if (retries++ < UBI_IO_RETRIES) {
-			dbg_io("error %d%s while reading %d bytes from PEB "
-			       "%d:%d, read only %zd bytes, retry",
-			       err, errstr, len, pnum, offset, read);
+			ubi_warn("error %d%s while reading %d bytes from PEB "
+				 "%d:%d, read only %zd bytes, retry",
+				 err, errstr, len, pnum, offset, read);
 			yield();
 			goto retry;
 		}
@@ -363,8 +363,8 @@ retry:
 	err = ubi->mtd->erase(ubi->mtd, &ei);
 	if (err) {
 		if (retries++ < UBI_IO_RETRIES) {
-			dbg_io("error %d while erasing PEB %d, retry",
-			       err, pnum);
+			ubi_warn("error %d while erasing PEB %d, retry",
+				 err, pnum);
 			yield();
 			goto retry;
 		}
@@ -382,7 +382,7 @@ retry:
 
 	if (ei.state == MTD_ERASE_FAILED) {
 		if (retries++ < UBI_IO_RETRIES) {
-			dbg_io("error while erasing PEB %d, retry", pnum);
+			ubi_warn("error while erasing PEB %d, retry", pnum);
 			yield();
 			goto retry;
 		}

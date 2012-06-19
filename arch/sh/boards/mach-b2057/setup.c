68a8f91e6b2db32e83f5825ce2a4572b6c556dd9
Angus CLARK <angus.clark@st.com>
2012-06-19 15:15:16 +0100

    stm: b2057: use stm-nand-bch driver for NAND support
    In most cases, it makes sense to use the FLEX NAND driver by default, since it
    works on all systems, and does not interfere with the manufacturer-programmed
    bad block markers.  However, the b2057 board is populated with a NAND device
    that is only supported by the stm-nand-bch driver (due to multi-bit ECC and
    Page/OOB geometry).  This patch updates the board setup and defconfig to select
    the stm-nand-bch driver.
    
    Signed-off-by: Angus Clark <angus.clark@st.com>
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>

--- arch/sh/boards/mach-b2057/setup.c
+++ arch/sh/boards/mach-b2057/setup.c
@@ -284,7 +284,7 @@ static int __init device_init(void)
 	stxh205_configure_mmc(0);
 
 	stxh205_configure_nand(&(struct stm_nand_config) {
-			.driver = stm_nand_flex,
+			.driver = stm_nand_bch,
 			.nr_banks = 1,
 			.banks = &b2057_nand_flash,
 			.rbn.flex_connected = 1,});

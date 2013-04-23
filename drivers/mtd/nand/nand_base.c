99db360c93d29055ca59c5e87710c9ee57afa99d
Angus CLARK <angus.clark@st.com>
2013-04-23 12:48:20 +0000

    mtd: nand: add diagnostic for no valid ONFI pages on ONFI NAND
    Generate an additional diagnostic "No valid ONFI param page found (bad CRC)", if
    when probing an ONFI-compatible NAND device, none of the three parameter pages
    are found to be valid.  This at least alerts the user to know that an ONFI
    device has been found, but we are ignoring all the ONFI information, and
    therefore falling back to non-ONFI heuristics.
    
    Signed-off-by: Angus Clark <angus.clark@st.com>

--- drivers/mtd/nand/nand_base.c
+++ drivers/mtd/nand/nand_base.c
@@ -2771,8 +2771,10 @@ static int nand_flash_detect_onfi(struct mtd_info *mtd, struct nand_chip *chip,
 		}
 	}
 
-	if (i == 3)
+	if (i == 3) {
+		printk(KERN_INFO "No valid ONFI param page found (bad CRC)\n");
 		return 0;
+	}
 
 	/* check version */
 	val = le16_to_cpu(p->revision);

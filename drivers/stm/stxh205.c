b7042d9b7ee8ac425b4e5284d483c03a3dcb332b
Angus CLARK <angus.clark@st.com>
2013-04-23 12:48:02 +0000

    stm: stxh205: update SPI-FSM capabilities
    Update the stxh205 SPI-FSM capabilities, in line with test observations:
    
    	- S/W reset appears to be functional, and even mandatory following boot
              in "FSM QUAD" mode. (Issue reported to Validation.)
    
    	- No locking issue found when polling the MODE_CHANGE register.
    
    Signed-off-by: Angus Clark <angus.clark@st.com>

--- drivers/stm/stxh205.c
+++ drivers/stm/stxh205.c
@@ -549,9 +549,9 @@ void __init stxh205_configure_spifsm(struct stm_plat_spifsm_data *data)
 	/* SoC/IP Capabilities */
 	data->capabilities.no_read_repeat = 1;
 	data->capabilities.no_write_repeat = 1;
-	data->capabilities.no_sw_reset = 1;
+	data->capabilities.no_sw_reset = 0;
 	data->capabilities.read_status_bug = spifsm_read_status_clkdiv4;
-	data->capabilities.no_poll_mode_change = 1;
+	data->capabilities.no_poll_mode_change = 0;
 	data->capabilities.boot_from_spi = (sysconf_read(sc) == 0x1a) ? 1 : 0;
 
 	sysconf_release(sc);

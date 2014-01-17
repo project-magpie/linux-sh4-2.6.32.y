1332fca40fb17b79cd80752395af3ebe857465ce
Francesco VIRLINZI <francesco.virlinzi@st.com>
2014-01-17 10:31:54 +0100

    stm: i2c: fixed timeout value which is in jiffies.
    [ linux-stm commit: 0454812efb8410547a502aa21e66508c62d5c71d ]
    
    This patch fixes the timeout value, which is supposed to be
    stored in jiffies, so that it can be modified by user space
    through the ioctl I2C_TIMEOUT.
    
    Update the .probe function so that the default timeout value
    is still 2 seconds.
    
    Reference: [Bugzilla 27605]
    
    Signed-off-by: Francesco Virlinzi <francesco.virlinzi@st.com>
    Reviewed-by: Shiraz Hashim <shiraz.hashim@st.com>
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>

--- drivers/i2c/busses/i2c-stm.c
+++ drivers/i2c/busses/i2c-stm.c
@@ -808,7 +808,7 @@ iic_xfer_retry:
 
 	timeout = wait_event_interruptible_timeout(adap->wait_queue,
 						   (transaction.waitcondition ==
-						    0), i2c_adap->timeout * HZ);
+						    0), i2c_adap->timeout);
 
 	local_irq_save(flag);
 
@@ -1177,7 +1177,7 @@ static int __init iic_stm_probe(struct platform_device *pdev)
 	}
 
 	platform_set_drvdata(pdev, i2c_stm);
-	i2c_stm->adapter.timeout = 2;
+	i2c_stm->adapter.timeout = 2 * HZ;
 	i2c_stm->adapter.retries = 0;
 	i2c_stm->adapter.class = I2C_CLASS_HWMON | I2C_CLASS_TV_ANALOG |
 		I2C_CLASS_TV_DIGITAL | I2C_CLASS_DDC | I2C_CLASS_SPD;;
